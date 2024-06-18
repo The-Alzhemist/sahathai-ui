a11a75c88f12c8dcffbccedbda5824f657155410
py405 <pchaiwatsupaporn@gmail.com>
2024-06-18 18:40:49 +0700

    chore: default tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,23 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        dark: {
+          DEFAULT: "#535353",
+          300: "#7A7A7A",
+          100: "#A1A1A1",
+          80: "#C8C8C8",
+          60: "#EFEFEF",
+        },
+        white: {
+          DEFAULT: "#FFFFFF",
+        },
+      },
+      boxShadow: {
+        1: "0px 1px 3px 0px #A6AFC366",
       },
     },
   },
