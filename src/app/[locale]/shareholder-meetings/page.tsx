46ddc00cc0b646970f67ff5fcddae32d81c02dcf
py405 <pchaiwatsupaporn@gmail.com>
2024-06-28 22:39:51 +0700

    feat(shareholder-meeting-page): mock ui

---
+++ src/app/[locale]/shareholder-meetings/page.tsx
@@ -0,0 +1,5 @@
+import { ShareholderMeetingPage } from '@/features/investorRelations/pages/ShareholderMeetingPage'
+
+export default function ShareholderMeetings() {
+  return <ShareholderMeetingPage />
+}
