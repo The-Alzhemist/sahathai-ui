7274bcd5a1482b63e93320a4126bef835443c6d1
pot-codework <chaiwat@codework-tech.com>
2024-11-27 15:34:09 +0700

    style(home): title banner

--- src/features/home/pages/HomePage/components/Banner/index.tsx
+++ src/features/home/pages/HomePage/components/Banner/index.tsx
@@ -23,10 +23,7 @@ export function Banner() {
         alt='เชื่อมโยง คุณสู่โลจิสติกส์ไทย'
       />
 
-      <section
-        ref={ref}
-        className='container mb-[13.33%] pl-[12.64%] z-[1] text-white'
-      >
+      <section ref={ref} className='mb-[13.33%] pl-[12.64%] z-[1] text-white'>
         <div
           className={cn(
             'transition-all duration-700 translate-x-[-200px] opacity-0',
