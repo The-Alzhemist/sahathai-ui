2cd9c8ea894a6c1f0ea51132b6ea428c45a0d610
Aphiwit1 <Aphiwit.boon@gmail.com>
2024-11-28 12:29:12 +0700

    feat/responsive-section-about-us

--- src/features/aboutUs/components/Vision/index.tsx
+++ src/features/aboutUs/components/Vision/index.tsx
@@ -9,7 +9,7 @@ export function Vision() {
   const t = useTranslations('AboutUsPage.VisionMissionValues.Vision')
 
   return (
-    <Animation className='mt-[90px] flex gap-[89px] justify-center max-w-[1065px] mx-auto'>
+    <Animation className='mt-[90px] flex flex-col-reverse  lg:flex-row px-5 md:px-3 justify-center gap-[89px] max-w-[1065px] mx-auto'>
       <Image
         className='shrink-0'
         src='/about-us/vision.png'
@@ -26,7 +26,7 @@ export function Vision() {
         <div className='w-full mt-[70px]'>
           <h2 className='headline-1 text-background'>{t('vision.title')}</h2>
           <Line className='my-[22px]' />
-          <p className='mt-[10px] text-[32px] leading-[48.38px] text-black-6 font-[300]'>
+          <p className='mt-[10px] text-xl md:text-[32px] leading-7 md:leading-[48.38px] text-black-6 font-[300]'>
             {t('vision.content')}
           </p>
         </div>
