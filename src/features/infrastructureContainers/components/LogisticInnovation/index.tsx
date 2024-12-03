619badaf339cdbe6ed9962c0248c1ed8226ec963
Aphiwit1 <aphiwit@codework-tech.com>
2024-12-03 15:47:27 +0700

    fix(InfrastructureContainerPage): responsive

--- src/features/infrastructureContainers/components/LogisticInnovation/index.tsx
+++ src/features/infrastructureContainers/components/LogisticInnovation/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export function LogisticInnovation() {
   return (
-    <Animation className='mt-[100px] max-w-[862px] w-full mx-auto'>
+    <Animation className='mt-[100px] max-w-[862px] w-full mx-auto px-5'>
       <div className='relative pt-[161.02%] w-full'>
         <Image
           src='/infrastructure-containers/logistic-innovation-th.png'
