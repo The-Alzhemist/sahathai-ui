3498f86cfb1377f2722589531f3262a9a0c3f551
Aphiwit1 <aphiwit@codework-tech.com>
2024-12-10 11:28:38 +0700

    Update page.tsx

--- src/app/[locale]/news/[slug]/page.tsx
+++ src/app/[locale]/news/[slug]/page.tsx
@@ -4,14 +4,14 @@ interface Post {
   content: string
 }
 
-// Next.js will invalidate the cache when a
-// request comes in, at most once every 600 seconds.(10 mins)
-export const revalidate = 60
+// // Next.js will invalidate the cache when a
+// // request comes in, at most once every 600 seconds.(10 mins)
+// export const revalidate = 60
 
-// We'll prerender only the params from `generateStaticParams` at build time.
-// If a request comes in for a path that hasn't been generated,
-// Next.js will server-render the page on-demand.
-export const dynamicParams = true // or false, to 404 on unknown paths
+// // We'll prerender only the params from `generateStaticParams` at build time.
+// // If a request comes in for a path that hasn't been generated,
+// // Next.js will server-render the page on-demand.
+// export const dynamicParams = false // or false, to 404 on unknown paths
 
 export async function generateStaticParams() {
   // const posts: Post[] = await fetch('https://api.vercel.app/blog').then(res =>
