d16a2bbca9451b8b9bf4b0c8782733a597816c1a
Aphiwit1 <aphiwit@codework-tech.com>
2024-12-17 12:41:59 +0700

    fix(storyblok): key

--- src/features/news/components/NewsBlogList/NewsBlogList.tsx
+++ src/features/news/components/NewsBlogList/NewsBlogList.tsx
@@ -1,18 +1,18 @@
 import { NewsCard } from '@/components/NewsCard'
 
-import { fetchData, fetchNewsBlogListData } from '@/libs/storyblok'
-import { StoryblokStory } from '@storyblok/react/rsc'
+import { fetchNewsBlogListData } from '@/libs/storyblok'
+
 import React from 'react'
 
 export default async function NewsBlogList() {
   const { data } = await fetchNewsBlogListData()
   const datas = data
-  console.log('data:::', data)
 
   return (
     <section className='max-w-[1200px] flex flex-wrap px-5 gap-5 mx-auto mb-10 flex-col md:flex-row justify-center items-center'>
       {datas.stories.map((newsBlog: any) => (
         <NewsCard
+          key={newsBlog._uid}
           title={newsBlog.content.body[0].newsTitle}
           content={newsBlog.content}
           createdAt={newsBlog.createdAt}
