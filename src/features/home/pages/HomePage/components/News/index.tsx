e62a16858acf8c698a09f77db295633e26e4c6cc
Aphiwit1 <aphiwit@codework-tech.com>
2024-12-18 11:19:11 +0700

    fix(storyblok): newsList in home page

--- src/features/home/pages/HomePage/components/News/index.tsx
+++ src/features/home/pages/HomePage/components/News/index.tsx
@@ -2,6 +2,7 @@ import { useTranslations } from 'next-intl'
 
 import { NewsCard } from '@/components/NewsCard'
 import { Animation } from '@/components/Animation'
+import LatestNewsHomePageList from '@/features/home/pages/HomePage/components/News/LatestNewsHomePageList/LatestNewsHomePageList'
 
 export function News() {
   const t = useTranslations('HomePage.News')
@@ -9,25 +10,8 @@ export function News() {
   return (
     <section className='container mt-[83px]'>
       <h2 className='headline-2 text-center text-navy'>{t('title')}</h2>
-      <Animation className='mt-[42px] flex flex-wrap justify-center gap-[32px] max-w-[862px] mx-auto'>
-        <NewsCard
-          title='SAHATHAI TERMINAL ร่วมออกบูธภายในพิธีลงนามข้อตกลงความร่วมมือเพื่อส่งเสริมศักยภาพและความร่วมมือการต่อต้านการค้าสัตว์ป่า ตัวนิ่ม สัตว์ป่าอื่น และพันธุ์พืชที่ผิดกฏหมาย'
-          content='Sahathai Terminal ร่วมออกบูธภายในพิธีลงนามข้อตกลงความร่วมมือเพื่อส่งเสริมศักยภาพและความร่วมมือการต่อต้านการค้าสัตว์ป่า ตัวนิ่ม สัตว์ป่าอื่น และพันธุ์พืชที่ผิดกฏหมาย ระหว่างสมาคมตัวแทนออกของรับอนุญาตไทย (Thai Authorized Customs Brokers Association: TACBA) และสมาคมสัตววิทยาแห่งลอนดอน (ZSL) ประเทศไทย ซึ่งจัดขึ้นในวันที่ 28 พฤษภาคมที่ผ่านมา ณ ห้องเดอะแซมเบอร์ ชั้น B โรงแรมเอส 31 สุขุมวิท กรุงเทพมหานคร'
-          createdAt='2010-11-12T13:14:15Z'
-          fullSlug='/xxx'
-        />
-        <NewsCard
-          title='SAHATHAI TERMINAL ร่วมออกบูธภายในพิธีลงนามข้อตกลงความร่วมมือเพื่อส่งเสริมศักยภาพและความร่วมมือการต่อต้านการค้าสัตว์ป่า ตัวนิ่ม สัตว์ป่าอื่น และพันธุ์พืชที่ผิดกฏหมาย'
-          content='Sahathai Terminal ร่วมออกบูธภายในพิธีลงนามข้อตกลงความร่วมมือเพื่อส่งเสริมศักยภาพและความร่วมมือการต่อต้านการค้าสัตว์ป่า ตัวนิ่ม สัตว์ป่าอื่น และพันธุ์พืชที่ผิดกฏหมาย ระหว่างสมาคมตัวแทนออกของรับอนุญาตไทย (Thai Authorized Customs Brokers Association: TACBA) และสมาคมสัตววิทยาแห่งลอนดอน (ZSL) ประเทศไทย ซึ่งจัดขึ้นในวันที่ 28 พฤษภาคมที่ผ่านมา ณ ห้องเดอะแซมเบอร์ ชั้น B โรงแรมเอส 31 สุขุมวิท กรุงเทพมหานคร'
-          createdAt='2010-11-12T13:14:15Z'
-          fullSlug='/xxx'
-        />
-        <NewsCard
-          title='SAHATHAI TERMINAL ร่วมออกบูธภายในพิธีลงนามข้อตกลงความร่วมมือเพื่อส่งเสริมศักยภาพและความร่วมมือการต่อต้านการค้าสัตว์ป่า ตัวนิ่ม สัตว์ป่าอื่น และพันธุ์พืชที่ผิดกฏหมาย'
-          content='Sahathai Terminal ร่วมออกบูธภายในพิธีลงนามข้อตกลงความร่วมมือเพื่อส่งเสริมศักยภาพและความร่วมมือการต่อต้านการค้าสัตว์ป่า ตัวนิ่ม สัตว์ป่าอื่น และพันธุ์พืชที่ผิดกฏหมาย ระหว่างสมาคมตัวแทนออกของรับอนุญาตไทย (Thai Authorized Customs Brokers Association: TACBA) และสมาคมสัตววิทยาแห่งลอนดอน (ZSL) ประเทศไทย ซึ่งจัดขึ้นในวันที่ 28 พฤษภาคมที่ผ่านมา ณ ห้องเดอะแซมเบอร์ ชั้น B โรงแรมเอส 31 สุขุมวิท กรุงเทพมหานคร'
-          createdAt='2010-11-12T13:14:15Z'
-          fullSlug='/xxx'
-        />
+      <Animation className='mt-[42px] flex flex-wrap justify-center gap-[32px] max-w-[1100px] mx-auto'>
+        <LatestNewsHomePageList />
       </Animation>
     </section>
   )
