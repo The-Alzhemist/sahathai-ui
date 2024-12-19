a3e48d6108909f0883a9d16f23ebc39dc968997f
Aphiwit1 <aphiwit@codework-tech.com>
2024-12-19 17:26:53 +0700

    feat(metaData): seo

--- src/app/[locale]/join-us/page.tsx
+++ src/app/[locale]/join-us/page.tsx
@@ -1,5 +1,40 @@
 import { JoinUsPage } from '@/features/joinUs/pages/JoinUsPage'
+import { getTranslations } from 'next-intl/server'
 
 export default function JoinUs() {
   return <JoinUsPage />
+}
+
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: {
+    locale: string
+  }
+}) {
+  const t = await getTranslations('MetaData')
+
+  return {
+    title: t('johnUs.Title'),
+    description: t('johnUs.Description'),
+    openGraph: {
+      title: t('johnUs.Title'),
+      description: t('johnUs.Description'),
+      images: [
+        {
+          url: 'https://sahathai-ui.vercel.app' + '/seo/meta-image-join-us.jpg',
+          width: 800,
+          height: 600,
+          alt: 'sahathai-join-us-meta-image',
+        },
+        {
+          url: 'https://sahathai-ui.vercel.app' + '/seo/meta-image-join-us.jpg',
+          width: 1800,
+          height: 1600,
+          alt: 'sahathai-join-us-meta-image',
+        },
+      ],
+    },
+    type: 'website',
+  }
 }
