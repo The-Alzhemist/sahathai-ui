f089761c085fdaea310aee117bde29031db29989
Aphiwit1 <aphiwit@codework-tech.com>
2024-12-25 15:55:43 +0700

    feat(mobileSidebar): list text

--- src/components/Header/components/MobileSidebar/MobileSidebar.tsx
+++ src/components/Header/components/MobileSidebar/MobileSidebar.tsx
@@ -19,11 +19,11 @@ const MobileSidebar = ({ handleOnToggle, isVisible }: MobileSidebarProps) => {
       ref={ref}
       className={cn(
         animationClassName,
-        'bg-gray-300 h-full w-full fixed top-0 left-0 z-20 p-5'
+        'bg-white h-full w-full fixed top-0 left-0 z-20 p-5'
       )}
     >
-      <div className='max-w-7xl mx-auto flex flex-col'>
-        <section className='flex justify-between'>
+      <div className='h-full max-w-7xl mx-auto flex flex-col'>
+        <section className='flex justify-between pb-2 border-b'>
           <Link href='/' className='shrink-0 p-[10px]'>
             <Image src='/logo.png' width={101} height={24} alt='' priority />
           </Link>
@@ -34,19 +34,59 @@ const MobileSidebar = ({ handleOnToggle, isVisible }: MobileSidebarProps) => {
             X
           </div>
         </section>
-        <div className='flex flex-col mt-10 gap-y-6 text-gray-700'>
-          <div onClick={() => handleOnToggle(false)}>
-            <Link href='/contact-us' className='flex items-center'>
-              {t('contactUs')}
-            </Link>
+        <section className='flex flex-col justify-between h-full'>
+          <div className='flex flex-col mt-10 gap-y-6 text-gray-700'>
+            <div onClick={() => handleOnToggle(false)}>
+              <Link href='/contact-us' className='flex items-center'>
+                {t('aboutUs')}
+              </Link>
+            </div>
+
+            <div onClick={() => handleOnToggle(false)}>
+              <Link className='flex items-center' href='/join-us'>
+                {t('service')}
+              </Link>
+            </div>
+
+            <div onClick={() => handleOnToggle(false)}>
+              <Link className='flex items-center' href='/join-us'>
+                {t('infra')}
+              </Link>
+            </div>
+
+            <div onClick={() => handleOnToggle(false)}>
+              <Link className='flex items-center' href='/news'>
+                {t('news')}
+              </Link>
+            </div>
+
+            <div onClick={() => handleOnToggle(false)}>
+              <Link className='flex items-center' href='/join-us'>
+                {t('eService')}
+              </Link>
+            </div>
+
+            <div onClick={() => handleOnToggle(false)}>
+              <Link className='flex items-center' href='/investor-information'>
+                {t('invester')}
+              </Link>
+            </div>
           </div>
 
-          <div onClick={() => handleOnToggle(false)}>
-            <Link className='flex items-center' href='/join-us'>
-              {t('joinUs')}
-            </Link>
+          <div className='flex flex-col mt-10 gap-y-6 text-gray-700'>
+            <div onClick={() => handleOnToggle(false)}>
+              <Link className='flex items-center' href='/join-us'>
+                {t('contactUs')}
+              </Link>
+            </div>
+
+            <div onClick={() => handleOnToggle(false)}>
+              <Link className='flex items-center' href='/investor-information'>
+                {t('joinUs')}
+              </Link>
+            </div>
           </div>
-        </div>
+        </section>
       </div>
     </nav>
   )
