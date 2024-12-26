b195db1e463bded71331ea1caf8efb0692d0eb0a
Aphiwit1 <aphiwit@codework-tech.com>
2024-12-26 10:29:32 +0700

    feat(storyblok): gen type

--- src/components/Storyblok/news/EventNewsDetailCard.tsx
+++ src/components/Storyblok/news/EventNewsDetailCard.tsx
@@ -3,6 +3,8 @@ import { renderRichText } from '@storyblok/react'
 import { richTextResolver, StoryblokRichTextOptions } from '@storyblok/richtext'
 import { ReactElement } from 'react'
 import React from 'react'
+import { EventNewsDetailCardStoryblok } from '../../../../component-types-sb'
+import Image from 'next/image'
 
 const resolvers = {
   image: (props: any) => {
@@ -15,19 +17,27 @@ const options: StoryblokRichTextOptions<ReactElement> = {
   keyedResolvers: true,
 }
 
-const EventNewsDetailCard: React.FC<any> = ({ blok }) => {
-  const html = richTextResolver(options).render(blok.newsDescription)
+interface EventNewsDetailCardProps {
+  blok: EventNewsDetailCardStoryblok
+}
+
+const EventNewsDetailCard: React.FC<any> = ({
+  blok,
+}: EventNewsDetailCardProps) => {
+  const html = blok.newsDescription
+    ? richTextResolver(options).render(blok.newsDescription as any)
+    : null
 
   return (
     <main {...storyblokEditable(blok)} className='mb-10'>
       <section className='bg-white flex flex-col p-3 md:p-10 rounded-[5px]'>
-        <img
-          src={
-            blok.newsImageCover
-              ? blok.newsImageCover.filename
-              : '/background.jpeg'
-          }
-          className='rounded-[5px] w-90% mx-auto mb-5'
+        <Image
+          src={blok.newsImageCover?.filename || '/background.jpeg'}
+          alt={blok.newsImageCover?.alt || 'Default background'}
+          width={blok.newsImageCover?.width || 800}
+          height={blok.newsImageCover?.height || 450}
+          className='rounded-[5px] mx-auto mb-5'
+          priority={true}
         />
         <div className='text-sm text-gray-600 mb-5'>{blok.newsDate}</div>
         <h2 className=' text-navy font-medium mb-3'>{blok.newsTitle}</h2>
