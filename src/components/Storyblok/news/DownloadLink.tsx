016077055c87cd681570ef98e1a2be89fb922271
Aphiwit1 <aphiwit@codework-tech.com>
2024-12-26 16:49:21 +0700

    fix(stpryblok): link

--- src/components/Storyblok/news/DownloadLink.tsx
+++ src/components/Storyblok/news/DownloadLink.tsx
@@ -2,24 +2,33 @@ import {
   storyblokEditable,
   StoryblokServerComponent,
 } from '@storyblok/react/rsc'
-import { ShowOneImageStoryblok } from '../../../../component-types-sb'
+import {
+  DownloadLinkStoryblok,
+  ShowOneImageStoryblok,
+} from '../../../../component-types-sb'
 import Image from 'next/image'
 
-const DownloadLink: React.FC<any> = props => {
+const DownloadLink: React.FC<DownloadLinkStoryblok> = props => {
   return (
     <main
       {...storyblokEditable(props)}
-      className='flex justify-start gap-x-3 items-center my-5'
+      className='flex w-fit justify-start gap-3 items-center my-5 flex-wrap border rounded-2xl p-3'
     >
-      <p className='text-primary font-semibold'>Lorem ipsum dolor sit.</p>
-      <a
-        className='bg-gray-900 text-white px-4 py-1 font-semibold rounded-full min-w-[40px]'
-        href=''
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Download
-      </a>
+      {props.linkURL ? (
+        <>
+          <p className='text-primary font-semibold'>{props.Title}</p>
+          <a
+            className='bg-gray-900 text-white px-4 py-1 font-semibold rounded-full min-w-[40px]'
+            href={props.linkURL}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Download
+          </a>
+        </>
+      ) : (
+        <p>No link title or url link</p>
+      )}
     </main>
   )
 }
