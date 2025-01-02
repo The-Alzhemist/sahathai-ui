6edb3f5bc6af523d3cbb597ce9f3a19ef136a042
Aphiwit1 <aphiwit@codework-tech.com>
2025-01-02 13:22:15 +0700

    feat(stock): data api

--- src/config/environtment.ts
+++ src/config/environtment.ts
@@ -2,3 +2,5 @@ export const STORYBLOK_TOKEN = 'H1wfrTArHm3VE441H8WQ5wtt'
 export const FORM_SPREE_ID = 'mbljnjjo'
 
 export const GTM_STAGING = 'GTM-PJ6XCKHT'
+
+export const EODHD_STOCK_TOKEN = '677622d53db520.53886203'
