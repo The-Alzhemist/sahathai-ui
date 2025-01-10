f4cb0ea18f94de9e3fd07c23be41910ac7934057
Aphiwit1 <aphiwit@codework-tech.com>
2025-01-10 10:17:55 +0700

    feat(updateLink): text constructure

---
+++ src/features/infrastructureContainers/components/OperationGuidelines/components/DockingInfo.tsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import Image from 'next/image'
+import { useLocale } from 'next-intl'
+
+export default function DockingInfo() {
+  const locale = useLocale()
+  return (
+    <section>
+      <Image
+        className='mb-5'
+        src='/infrastructure-containers/docking.png'
+        alt=''
+        width={666}
+        height={328}
+      />
+      {locale === 'th' && <DockingInfoTH />}
+      {locale === 'en' && <DockingInfoEN />}
+      {locale === 'cn' && <DockingInfoCN />}
+    </section>
+  )
+}
+
+export const DockingInfoTH = () => {
+  return (
+    <>
+      <p className='text-gray-800 text-sm mb-3'>
+        สายการเดินเรือหรือตัวแทนสายการเดินเรือ
+        ต้องส่งอีเมล์รายละเอียดของเรือบรรทุกสินค้า บริการ
+        และลักษณะของเรือบรรทุกสินค้า (แผนการจัดเก็บทั่วไป แผนการผูกยึดเรือ
+        แผนการจัดวางตู้สินค้าเย็น ระบบความปลอดภัย
+        และตารางพื้นที่ระวางตู้คอนเทนเนอร์) ให้แก่ สหไทย เทอร์มินอล
+        ล่วงหน้าอย่างน้อย 1 สัปดาห์
+        ก่อนเข้าเทียบท่าสายการเดินเรือหรือตัวแทนสายการเดินเรือ
+        ต้องส่งข้อมูลตารางการเข้าเทียบท่าระยะยาวล่วงหน้าอย่างน้อย 1 เดือน
+        ให้กับสหไทย เทอร์มินอล เพื่อให้สหไทย
+        เทอร์มินอลวางแผนจองตู้คอนเทนเนอร์ให้กับเรือขนส่งสินค้าล่วงหน้าอย่างน้อย
+        7, 3 และ 1 วัน ก่อนเข้าเทียบท่า สายการเดินเรือหรือตัวแทนสายการเดินเรือ
+        ต้องส่งข้อมูลประมาณการของวันที่เรือจะเข้าเทียบท่า (ETA)
+        และปริมาณสินค้าที่จะทำการโหลดขึ้นหรือลงโดยประมาณ เพื่อให้ สหไทย
+        เทอร์มินอล ยืนยันแผนการเข้าเทียบท่า (POB)
+        และประมาณการของวันที่ที่เรือจะออกจากท่าเรือต้นทาง (ETD)
+      </p>
+
+      <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-2'>
+        <span className='text-sm text-gray-700'>
+          ส่งอีเมล์ถึงสำนักงานปฏิบัติการของ สหไทย เทอร์มินอล ได้ที่
+        </span>
+
+        <span className='text-primary-1'>planner.stt@sahathai.com</span>
+      </div>
+    </>
+  )
+}
+
+export const DockingInfoEN = () => {
+  return (
+    <>
+      <p className='text-gray-800 text-sm mb-3'>
+        Shipping lines or shipping agents must send an email with the details of
+        the cargo ship, services, and cargo ship characteristics (general
+        stowage plan, lashing plan, refrigerated container arrangement plan,
+        safety system, and container slot allocation plan) to Sahathai Terminal
+        at least one week in advance. Shipping lines or shipping agents must
+        provide long-term berthing schedules at least one month in advance for
+        planning and pre-booking of container slots 7, 3, and 1 day before
+        berthing. The shipping lines or agents must also provide an estimated
+        time of arrival (ETA) and estimated cargo volumes for loading or
+        unloading. Sahathai Terminal will confirm the berthing plan (POB) and
+        the estimated departure time (ETD) from the terminal.
+      </p>
+
+      <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-2'>
+        <span className='text-sm text-gray-700'>
+          Please email the operations office of Sahathai Terminal at
+        </span>
+
+        <span className='text-primary-1'>planner.stt@sahathai.com</span>
+      </div>
+    </>
+  )
+}
+
+export const DockingInfoCN = () => {
+  return (
+    <>
+      <p className='text-gray-800 text-sm mb-3'>
+        船公司或船公司代理必须提前至少 1
+        周发送电子邮件，提供货船的详细信息、服务和货船特性（一般装载计划、
+        系泊计划、冷藏集装箱布置计划、安全系统以及集装箱插槽分配计划）到
+        Sahathai Terminal。船公司或船公司代理 必须提前至少 1
+        个月提供长期停泊时间表，以便在停泊前 7、3 和 1
+        天预订集装箱插槽。此外，船公司或代理人还
+        必须提供预计到达时间（ETA）和预计装卸货物的数量。Sahathai Terminal
+        将确认停泊计划（POB）以及预计的 离港时间（ETD）。
+      </p>
+
+      <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-2'>
+        <span className='text-sm text-gray-700'>
+          请发送电子邮件至 Sahathai Terminal 的运营办公室，邮箱为
+        </span>
+
+        <span className='text-primary-1'>planner.stt@sahathai.com</span>
+      </div>
+    </>
+  )
+}
---
+++ src/features/infrastructureContainers/components/OperationGuidelines/components/HazardousGoods.tsx
@@ -0,0 +1,162 @@
+import React from 'react'
+import Image from 'next/image'
+import { useLocale } from 'next-intl'
+
+export default function HazardousGoods() {
+  const locale = useLocale()
+  return (
+    <section>
+      <Image
+        className='mb-5'
+        src='/infrastructure-containers/docking.png'
+        alt=''
+        width={666}
+        height={328}
+      />
+      {locale === 'th' && <HazardousGoodsInfoTH />}
+      {locale === 'en' && <HazardousGoodsInfoEN />}
+      {locale === 'cn' && <HazardousGoodsInfoCN />}
+    </section>
+  )
+}
+
+const HazardousGoodsInfoTH = () => {
+  return (
+    <>
+      <p className='text-gray-800 text-sm mb-3'>
+        สินค้าอันตราย ท่าเรือจะใช้ UN Number
+        ในการตรวจสอบและบ่งชี้ประเภทสินค้าอันตราย โดยสามารถแบ่ง
+        ประเภทสินค้าอันตราย ได้ออกเป็น 3 กลุ่ม ดังนี้
+      </p>
+      <ul className='flex flex-col gap-2 text-gray-800 text-sm mb-3'>
+        <li className='mb-3 flex items-center gap-2'>
+          <span className='bg-red-200 rounded-xl px-3 py-1'>กลุ่มที่ 1 </span>
+          <span>ห้ามขนถ่าย</span>
+        </li>
+
+        <li className='mb-3 flex items-center gap-2'>
+          <span className='bg-yellow-200 rounded-xl px-3 py-1'>
+            กลุ่มที่ 2{' '}
+          </span>
+          <span>ไม่รับฝากเก็บ (ลูกค้าต้องนำรถมารับที่ข้างเรือเท่านั้น)</span>
+        </li>
+
+        <li className='mb-3 flex items-center gap-2'>
+          <span className='bg-green-200 rounded-xl px-3 py-1'>กลุ่มที่ 3 </span>
+          <span>
+            ฝากเก็บได้ไม่เกิน 5 วันทำการ ลูกค้าต้องนำรถมารับภายใน 5 วัน
+            (นับจากวันที่ตู้วางพื้นในลาน)
+          </span>
+        </li>
+      </ul>
+
+      <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-1'>
+        <span className='text-sm text-gray-700'>
+          สามารถ นำ UN Number เข้าไปตรวจสอบข้อมูลในระบบ โดยเข้าไปตรวจสอบใน
+        </span>
+
+        <a
+          className='text-primary-1'
+          href='https://stt.sahathaiterminal.com/containerinquery/Default.aspx?type=DG'
+          rel='noopener'
+          target='_blank'
+        >
+          ลิ้งตรวจสอบข้อมูลในระบบ
+        </a>
+      </div>
+    </>
+  )
+}
+
+const HazardousGoodsInfoEN = () => {
+  return (
+    <>
+      <p className='text-gray-800 text-sm mb-3'>
+        Hazardous goods are identified and verified using their UN Number. They
+        can be categorized into three groups as follows:
+      </p>
+      <ul className='flex flex-col gap-2 text-gray-800 text-sm mb-3'>
+        <li className='mb-3 flex items-center gap-2'>
+          <span className='bg-red-200 rounded-xl px-3 py-1'>Group 1 </span>
+          <span>Prohibited for handling.</span>
+        </li>
+
+        <li className='mb-3 flex items-center gap-2'>
+          <span className='bg-yellow-200 rounded-xl px-3 py-1'>Group 2 </span>
+          <span>
+            Not accepted for storage (customers must collect directly from the
+            vessel).
+          </span>
+        </li>
+
+        <li className='mb-3 flex items-center gap-2'>
+          <span className='bg-green-200 rounded-xl px-3 py-1'>Group 3 </span>
+          <span>
+            Can be stored for up to 5 working days. Customers must collect
+            within 5 days (starting from the date the container is placed in the
+            yard).
+          </span>
+        </li>
+      </ul>
+
+      <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-1'>
+        <span className='text-sm text-gray-700'>
+          You can check the UN Number details in the system by visiting:
+        </span>
+
+        <a
+          className='text-primary-1'
+          href='https://stt.sahathaiterminal.com/containerinquery/Default.aspx?type=DG'
+          rel='noopener'
+          target='_blank'
+        >
+          check the UN Number details
+        </a>
+      </div>
+    </>
+  )
+}
+
+const HazardousGoodsInfoCN = () => {
+  return (
+    <>
+      <p className='text-gray-800 text-sm mb-3'>
+        危险品通过 UN 编号进行识别和验证，可分为以下三类：
+      </p>
+      <ul className='flex flex-col gap-2 text-gray-800 text-sm mb-3'>
+        <li className='mb-3 flex items-center gap-2'>
+          <span className='bg-red-200 rounded-xl px-3 py-1'>第一类 </span>
+          <span>禁止装卸。</span>
+        </li>
+
+        <li className='mb-3 flex items-center gap-2'>
+          <span className='bg-yellow-200 rounded-xl px-3 py-1'>第二类 </span>
+          <span>不接受存储（客户必须直接从船只提取）。</span>
+        </li>
+
+        <li className='mb-3 flex items-center gap-2'>
+          <span className='bg-green-200 rounded-xl px-3 py-1'>第三类 </span>
+          <span>
+            可存储最多 5 个工作日。客户必须在 5
+            天内提取（从集装箱放置到堆场的日期算起）。
+          </span>
+        </li>
+      </ul>
+
+      <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-1'>
+        <span className='text-sm text-gray-700'>
+          您可以访问以下链接，在系统中检查 UN 编号的详细信息：
+        </span>
+
+        <a
+          className='text-primary-1'
+          href='https://stt.sahathaiterminal.com/containerinquery/Default.aspx?type=DG'
+          rel='noopener'
+          target='_blank'
+        >
+          在系统中检查 UN 编号的详细信息
+        </a>
+      </div>
+    </>
+  )
+}
