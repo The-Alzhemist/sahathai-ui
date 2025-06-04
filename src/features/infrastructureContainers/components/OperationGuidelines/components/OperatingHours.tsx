dd8ca1119c008143f8ce3a82a40ce31e50b97f4b
Aphiwit1 <aphiwit@codework-tech.com>
2025-06-04 15:37:55 +0700

    feat(structurePage): update content

--- src/features/infrastructureContainers/components/OperationGuidelines/components/OperatingHours.tsx
+++ src/features/infrastructureContainers/components/OperationGuidelines/components/OperatingHours.tsx
@@ -6,13 +6,13 @@ export default function OperatingHours() {
   const locale = useLocale()
   return (
     <section>
-      {/* <Image
+      { <Image
         className='mb-5'
-        src='/infrastructure-containers/docking.png'
+        src='/infrastructure-containers/operating-hours.jpg'
         alt=''
         width={666}
         height={328}
-      /> */}
+      /> }
       {locale === 'th' && <OperatingHoursInfoTH />}
       {locale === 'en' && <OperatingHoursInfoEN />}
       {locale === 'cn' && <OperatingHoursInfoCN />}
@@ -23,17 +23,43 @@ export default function OperatingHours() {
 const OperatingHoursInfoTH = () => {
   return (
     <>
-      <p className='text-gray-800 text-sm mb-3'>
-        แผนกปฏิบัติการทำงาน 7 วัน 24 ชม.
-      </p>
+        <ul className='flex flex-col gap-y-5 list-disc'>
+            <li className='text-gray-600 text-sm'>วันทำการ: จันทร์-เสาร์ เวลาทำการ: 08:00-17:00 น.
+            </li>
+
+            <li className='text-gray-600 text-sm'>ตู้คอนเทนเนอร์ทั่วไปที่ขนส่งด้วยรถบรรทุก: 24
+                ชั่วโมงก่อนเวลาเข้าเทียบท่า (ATB)
+            </li>
+
+            <li className='text-gray-600 text-sm'>ตู้สินค้าเย็น (สินค้าประเภทผลไม้): 1 ชั่วโมงก่อน ATB
+            </li>
+
+            <li className='text-gray-600 text-sm'>สินค้าอันตราย ประเภทที่ 2 (DG GROUP2): ขนส่งตรงจากเรือ/แชสซี ไปยัง
+                แชสซี/เรือ
+            </li>
+
+            <li className='text-gray-600 text-sm'>สินค้าอันตราย ประเภทที่ 3 (DG GROUP3):
+                สามารถจัดเก็บสินค้าไว้ที่ท่าเรือได้สูงสุด 48
+                ชั่วโมงหลังจากโหลดสินค้าลงจากเรือขนส่งสินค้า (หรือ) 48
+                ชั่วโมงก่อนเวลาปฏิบัติการของสถานีบรรจุหรือส่งมอบสินค้าที่ต้นทาง (CFS)
+            </li>
+
+
+            <li className='text-red-600 text-sm'>หากมีความประสงค์ในการทำงานล่วงเวลา จำเป็นต้องแจ้งก่อนเวลา 16.00 น.
+                ในวันทำการ
+            </li>
+
+            <li className='text-red-600 text-sm'>เวลาปิดรับสินค้าประเภท CFS: 6 ชั่วโมง ก่อนเวลาปิดรับสินค้าของ CY
+            </li>
+        </ul>
     </>
   )
 }
 
 const OperatingHoursInfoEN = () => {
-  return (
-    <>
-      <p className='text-gray-800 text-sm mb-3'>
+    return (
+        <>
+            <p className='text-gray-800 text-sm mb-3'>
         The operations department works 7 days a week, 24 hours a day.
       </p>
     </>
