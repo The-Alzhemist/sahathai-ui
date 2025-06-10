51b8f7804bab33838747d79d7fb7c4bd1a2eef41
Aphiwit1 <aphiwit@codework-tech.com>
2025-06-10 13:06:51 +0700

    feat(JohnUS):pdf file

--- src/features/joinUs/components/Policy/index.tsx
+++ src/features/joinUs/components/Policy/index.tsx
@@ -8,19 +8,19 @@ export function Policy() {
 
   return (
     <Animation className='max-w-[896px] mx-auto space-y-[32px] shadow-2 rounded-[8px] p-[24px]'>
-      <PolicyCard title={t('workplaceSafetyHygienePolicy')} downloadLink='' />
-      <PolicyCard title={t('employeeCompensationPolicy')} downloadLink='' />
+      <PolicyCard title={t('workplaceSafetyHygienePolicy')} downloadLink='https://storage.googleapis.com/sahathai-prod/about-us-page/ข้อ8.1นโยบายและแนวปฏิบัติเกี่ยวกับความปลอดภัย-และสุขอนามัยในสถานที่ทำงาน.pdf' />
+      <PolicyCard title={t('employeeCompensationPolicy')} downloadLink='https://storage.googleapis.com/sahathai-prod/about-us-page/ข้อ8.2นโยบายอัตราค่าตอบแทนพนักงาน-04032019.pdf' />
       <PolicyCard
         title={t('companyPersonnelDevelopmentPolicy')}
-        downloadLink=''
+        downloadLink='https://storage.googleapis.com/sahathai-prod/about-us-page/ข้อ8.3-นโยบายการพัฒนาบุคลากรของบริษัท.pdf'
       />
       <PolicyCard
         title={t('humanResourcesLaborRelationsPolicy')}
-        downloadLink=''
+        downloadLink='https://storage.googleapis.com/sahathai-prod/about-us-page/ข้อ8.4-นโยบายการบริหารทรัพยากรบุคคล-และแรงงานสัมพันธ์.pdf'
       />
       <PolicyCard
         title={t('measuresPreventSpreadCovid19Virus')}
-        downloadLink=''
+        downloadLink='https://storage.googleapis.com/sahathai-prod/about-us-page/Covid-19-1.pdf'
       />
     </Animation>
   )
