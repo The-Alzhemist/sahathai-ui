f3bed255b7612f780e4b897dc2ec8577e6b1cddb
Aphiwit1 <aphiwit@codework-tech.com>
2025-06-17 12:12:56 +0700

    fix(image): logistic-innovation-th-4

--- src/features/infrastructureContainers/components/LogisticInnovation/index.tsx
+++ src/features/infrastructureContainers/components/LogisticInnovation/index.tsx
@@ -6,7 +6,7 @@ export function LogisticInnovation() {
     <Animation className='mt-[100px] max-w-[862px] w-full mx-auto px-5'>
       <div className='relative pt-[161.02%] w-full mb-10'>
         <Image
-          src='/infrastructure-containers/logistic-innovation-th-3.png'
+          src='/infrastructure-containers/logistic-innovation-th-4.png'
           fill
           alt=''
         />
