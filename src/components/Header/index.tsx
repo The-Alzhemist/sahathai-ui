6803d3c8d34184e990346f86c84cd29820c40bf4
niti-jira <niti@alzhemist.com>
2025-07-15 13:58:07 +0700

    fix(navbar): hide on scroll

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,35 +8,38 @@ import { SearchInput } from './SearchInput'
 import { ListIcon } from '@/components/icons/ListIcon'
 import { useState } from 'react'
 import MobileSidebar from '@/components/Header/components/MobileSidebar/MobileSidebar'
+import { useHideOnScroll } from '@/hooks/useHideOnScroll'
 
 export function Header() {
   const t = useTranslations('Header')
   const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false)
+  const isVisible = useHideOnScroll()
 
   const handleOnToggleMobileSidebar = (isOpen: boolean) => {
     setIsMobileSidebarOpen(isOpen)
   }
 
   return (
-    <div className='shadow-1 sticky top-0 bg-white z-20'>
-      <div className='px-[10px] py-[8px] flex justify-between items-center container'>
-        <Link href='/' className='shrink-0 p-[10px]'>
-          <Image src='/logo.png' width={101} height={24} alt='' priority />
+    <div
+      className={`shadow-1 sticky top-0 bg-white z-20 transition-transform duration-300  ${isVisible ? 'translate-y-0' : 'translate-y-[-150%] pointer-events-none'}`}>
+      <div className="px-[10px] py-[8px] flex justify-between items-center container">
+        <Link href="/" className="shrink-0 p-[10px]">
+          <Image src="/logo.png" width={101} height={24} alt="" priority />
         </Link>
 
-        <section className='flex gap-x-5'>
-          <div className='sm:hidden'>
+        <section className="flex gap-x-5">
+          <div className="sm:hidden">
             <LocaleButton />
           </div>
 
-          <ul className='flex justify-end items-center w-full medium text-background gap-[42px] mobile:hidden mr-10'>
+          <ul className="flex justify-end items-center w-full medium text-background gap-[42px] mobile:hidden mr-10">
             <li>
-              <Link href='/contact-us' className='flex items-center'>
+              <Link href="/contact-us" className="flex items-center">
                 {t('contactUs')}
               </Link>
             </li>
             <li>
-              <Link className='flex items-center' href='/join-us'>
+              <Link className="flex items-center" href="/join-us">
                 {t('joinUs')}
               </Link>
             </li>
@@ -51,10 +54,10 @@ export function Header() {
           </button> */}
           </ul>
           <button
-            className='block sm:hidden'
+            className="block sm:hidden"
             onClick={() => handleOnToggleMobileSidebar(true)}
           >
-            <ListIcon width='25' height='25' />
+            <ListIcon width="25" height="25" />
           </button>
         </section>
       </div>
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,14 +2,17 @@
 
 import { useSubmenu } from '@/hooks/useSubmenu'
 import { Link, usePathname } from '@/libs/intl/navigation'
+import { useHideOnScroll } from '@/hooks/useHideOnScroll'
 
 export function Menu() {
   const { menus } = useSubmenu()
   const pathname = usePathname()
+  const isVisible = useHideOnScroll()
 
   return (
-    <div className='fixed top-[60px] bg-background text-white  md:rounded-b-[30px] w-full z-10 '>
-      <ul className=' flex justify-start md:justify-center overflow-x-scroll  no-scrollbar  '>
+    <div
+      className={`sticky top-[60px] bg-background text-white md:rounded-b-[30px] w-full z-10 transition-all duration-300 ${isVisible ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0 pointer-events-none'}`}>
+      <ul className=" flex justify-start md:justify-center overflow-x-scroll  no-scrollbar  ">
         {menus.map(menu => {
           const isActive = pathname === menu.pathname
 
@@ -20,8 +23,8 @@ export function Menu() {
                   className={`headline-6 !font-[300] block px-[24px] py-[18px] hover:bg-primary-1 whitespace-nowrap ${
                     isActive ? 'bg-primary-1' : ''
                   }`}
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  target="_blank"
+                  rel="noopener noreferrer"
                   href={menu.pathname}
                 >
                   {menu.title}
