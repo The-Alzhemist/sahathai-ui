f018031c53605cefee66faa30334b88b7bec6cc3
Aphiwit1 <aphiwit@codework-tech.com>
2025-07-16 09:36:09 +0700

    fix(ShareHolderMeeting): add mock component

--- src/app/[locale]/investor-information/share-holder/page.tsx
+++ src/app/[locale]/investor-information/share-holder/page.tsx
@@ -1,5 +1,4 @@
-import { REVALIDATE_TIME } from '@/config/environtment'
-import { InvestorInformationPage } from '@/features/investorRelations/pages/InvestorInformationPage'
+
 import { ShareHolderPage } from '@/features/investorRelations/pages/ShareHolderPage'
 
 
