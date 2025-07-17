c5f96a2a6eb7a55797e0c83c48933c3d9263e16b
Aphiwit1 <aphiwit@codework-tech.com>
2025-07-17 13:03:08 +0700

    feat(FinancialInformation):tab

---
+++ src/features/investorRelations/pages/FinancialInformationPage/component/Docmuments/Financial2567.tsx
@@ -0,0 +1,32 @@
+import { AccordionItem } from '@/features/investorRelations/pages/ShareHolderMeetingPage/component/AccordientList'
+
+export const financial2567 = (
+  t: (key: string, values?: Record<string, any>) => string,
+): AccordionItem[] => {
+  const year = 2567
+  return [
+    {
+      title: t('title', { year }),
+      files: [
+        {
+          title: t('quarter.one'),
+          name: 'invite-2568.pdf',
+          type: 'pdf',
+        },
+        {
+          title: t('quarter.two'),
+          name: 'presentation.pdf',
+        },
+        {
+          title: t('quarter.three'),
+          name: 'presentation.pdf',
+        },
+        {
+          title: t('quarter.four'),
+          name: 'presentation.pdf',
+        },
+      ],
+    },
+
+  ];
+};
