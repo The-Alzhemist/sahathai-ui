47d7f240c95771349838ab328e4ae59e28819f73
Aphiwit1 <aphiwit@codework-tech.com>
2025-08-05 16:31:12 +0700

    feat(PerformanceReport): use accordionTab

--- src/app/[locale]/investor-information/performance-report/page.tsx
+++ src/app/[locale]/investor-information/performance-report/page.tsx
@@ -1,6 +1,21 @@
 
 import { PerformanceReportPage } from '@/features/investorRelations/pages/PerformanceReportPage/PerformanceReportPage'
+import { fetchStoryblokStory } from '@/utils/storyblok'
 
-export default async function PerformanceReport() {
-  return <PerformanceReportPage/>
+export default async function PerformanceReport({
+                                                  params,
+                                                }: {
+  params: { locale: string }
+}) {
+
+  const { locale } = params
+
+  const response = await fetchStoryblokStory(
+    'invrester-relartion/1c52b131-12fd-4d51-9daa-dccd33b4e410',
+    locale,
+    'draft'
+  )
+
+
+  return <PerformanceReportPage performanceReportData={response} />
 }
