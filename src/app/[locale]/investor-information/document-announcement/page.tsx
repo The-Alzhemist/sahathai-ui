a4510d4c3b74e88569ee5bf226d7fb2a31ecbcf0
Aphiwit1 <aphiwit@codework-tech.com>
2025-08-06 13:54:26 +0700

    feat(DocumentAnnouncement):redesign

--- src/app/[locale]/investor-information/document-announcement/page.tsx
+++ src/app/[locale]/investor-information/document-announcement/page.tsx
@@ -2,7 +2,21 @@
 import {
   DocumentAnnouncementPage
 } from '@/features/investorRelations/pages/DocumentAnnouncementPage/DocumentAnnouncementPage'
+import { fetchStoryblokStory } from '@/utils/storyblok'
 
-export default async function DocumentAnnouncement() {
-  return <DocumentAnnouncementPage/>
+export default async function DocumentAnnouncement({
+                                                     params,
+                                                   }: {
+  params: { locale: string }
+}) {
+
+  const { locale } = params
+
+  const response = await fetchStoryblokStory(
+    'invrester-relartion/ed6b6179-86fc-473f-8188-81827cb9b1e8',
+    locale,
+    'draft'
+  )
+
+  return <DocumentAnnouncementPage  data={response} />
 }
