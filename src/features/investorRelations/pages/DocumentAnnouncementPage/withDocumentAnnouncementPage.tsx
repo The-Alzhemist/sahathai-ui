054e2b6a2ad3a487e65fefe455b5ccaa0c2c1ff4
Aphiwit1 <aphiwit@codework-tech.com>
2025-08-06 13:54:26 +0700

    feat(DocumentAnnouncement):redesign

--- src/features/investorRelations/pages/DocumentAnnouncementPage/withDocumentAnnouncementPage.tsx
+++ src/features/investorRelations/pages/DocumentAnnouncementPage/withDocumentAnnouncementPage.tsx
@@ -1,11 +1,14 @@
 import { FunctionComponentType } from '@/models/FunctionComponentType'
+import { FinancialInformationPageProps } from '@/features/investorRelations/pages/FinancialInformationPage/interface'
+import { DocumentAnnouncementPageProps } from '@/features/investorRelations/pages/DocumentAnnouncementPage/interface'
 
 export function withDocumentAnnouncementPage(
-  Component: React.FC<FunctionComponentType>
+  Component: React.FC<DocumentAnnouncementPageProps>
 ) {
-  function withShareHolder() {
-    return <Component />
+  function withShareHolder(props:DocumentAnnouncementPageProps) {
+    return <Component {...props} />
   }
+
 
   return withShareHolder
 }
