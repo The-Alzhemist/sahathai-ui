4fb309202aaf6ce8df5bf2bc5b6ce9040ffd6c84
niti-jira <niti@alzhemist.com>
2025-08-07 11:18:03 +0700

    fix(aboutus): organize structure

--- src/features/aboutUs/components/CorporateGroupOrganizationalStructure/index.tsx
+++ src/features/aboutUs/components/CorporateGroupOrganizationalStructure/index.tsx
@@ -8,31 +8,42 @@ export function CorporateGroupOrganizationalStructure() {
   const t = useTranslations('AboutUsPage.CorporateGroupOrganizationalStructure')
 
   return (
-    <section className='mt-[80px] mb-[134px] max-w-[1040px] w-full mx-auto px-5'>
-      <h2 className='text-navy headline-2'>{t('corporateGroup')}</h2>
-      <Line className='my-[8px]' />
-      <Animation className='w-full mt-[50px] shadow-8 rounded-[15px] overflow-hidden'>
-        <div className='relative pt-[55.77%] w-full'>
-          <Image src='/about-us/corporate-group.png' fill alt='' />
-        </div>
-      </Animation>
+    <section className='mt-[80px] w-full relative'>
+      <div className='pb-[134px] max-w-[1040px] w-full mx-auto px-5'>
+        <h2 className='text-navy headline-2'>{t('corporateGroup')}</h2>
+        <Line className='my-[8px]' />
+        <Animation className='w-full mt-[50px] shadow-8 rounded-[15px] overflow-hidden'>
+          <div className='relative pt-[55.77%] w-full'>
+            <Image src='/about-us/corporate-group.png' fill alt='' />
+          </div>
+        </Animation>
 
-      <h2 className='mt-[110px] headline-2 text-navy'>
-        {t('organizationalStructure.title')}
-      </h2>
-      <Line className='my-[8px]' />
-      <p className='mt-[20px] text-black-6 body-1 whitespace-pre-line'>
+        <h2 className='mt-[110px] headline-2 text-navy'>
+          {t('organizationalStructure.title')}
+        </h2>
+        <Line className='my-[8px]' />
+        <p className='mt-[20px] text-black-6 body-1 whitespace-pre-line'>
+          {t.rich(`organizationalStructure.content`, {
+            name: chunks => (
+              <div className='inline-flex font-semibold  pr-5'>{chunks}</div>
+            ),
+          })}
+        </p>
 
-        {t.rich(`organizationalStructure.content`, {
-          name: (chunks) => <div className='inline-flex font-semibold  pr-5'>{chunks}</div>,
-        })}
-      </p>
-
-      <Animation className='w-full mt-[50px] rounded-[15px] shadow-8 overflow-hidden'>
-        <div className='relative pt-[77.31%] w-full'>
-          <Image src='/about-us/organizational-structure.png' fill alt='' />
-        </div>
-      </Animation>
+        <Animation className='w-full mt-[50px] rounded-[15px] shadow-8 overflow-hidden'>
+          <div className='relative pt-[77.31%] w-full'>
+            <Image src='/about-us/organizational-structure.png' fill alt='' />
+          </div>
+        </Animation>
+      </div>
+      <div className='absolute -z-10 bottom-0 left-0 h-[704px] w-full'>
+        <Image
+          src='/about-us/bg.png'
+          alt='About us background'
+          fill
+          className='object-bottom'
+        />
+      </div>
     </section>
   )
 }
