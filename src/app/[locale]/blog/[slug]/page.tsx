df62a1901e9a1bd677cee3032a0ad1590f06214e
Aphiwit1 <aphiwit@codework-tech.com>
2025-08-08 08:27:33 +0700

    feat(Blog):   mock

---
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -0,0 +1,118 @@
+import { REVALIDATE_TIME } from '@/config/environtment'
+import { Link } from '@/libs/intl/navigation'
+import { fetchDataBySlug, fetchNewsBlogListData } from '@/libs/storyblok'
+import { StoryblokStory } from '@storyblok/react/rsc'
+import { useTranslations } from 'next-intl'
+import { getTranslations } from 'next-intl/server'
+
+export const revalidate = 300 // 5 min
+export const dynamicParams = true // or false, to 404 on unknown paths
+
+export async function generateStaticParams() {
+  const locales = ['th', 'en', 'cn']
+
+  // Fetch slugs for each locale
+  const slugs = await Promise.all(
+    locales.map(async locale => {
+      const { data } = await fetchNewsBlogListData(1, 99, locale) // Pass locale as lang
+      return data.stories.map((d: any) => ({
+        slug: d.slug,
+        locale: locale,
+      }))
+    })
+  )
+
+  return slugs.flat()
+}
+
+export default async function Page({ params }: { params: any }) {
+  const { slug, locale } = params
+  const { data } = await fetchDataBySlug(slug, locale)
+  const t = await getTranslations('NewsPage')
+
+  return (
+    <section className='relative flex-col'>
+      <section className='max-w-[990px] px-5 mx-auto mt-[80px]'>
+        <div className='flex justify-between mb-8'>
+          <h1 className='text-xl md:text-3xl text-navy'>
+            {t('PageContent.Title')}
+          </h1>
+          <button className='text-sm text-gray-500 border border-gray-500 px-4 py-1 rounded-3xl'>
+            <Link href='/news'> {t('PageContent.Back')}</Link>
+          </button>
+        </div>
+
+        <StoryblokStory story={data?.story} />
+      </section>
+    </section>
+  )
+}
+
+export async function generateMetadata({
+  params: { slug, locale },
+}: {
+  params: {
+    locale: string
+    slug: string
+  }
+}) {
+  const { data } = await fetchDataBySlug(slug, locale)
+
+  const body = data.story.content.body
+  if (!body || body.length === 0) {
+    return {
+      title: 'Sahathai | blog',
+      description: '',
+      openGraph: {
+        title: 'Sahathai | blog',
+        description: '',
+        images: [
+          {
+            url: 'https://sahathai-ui.vercel.app/seo/meta-image-home.jpg',
+            width: 800,
+            height: 600,
+            alt: 'sahathai-meta-image-blog',
+          },
+          {
+            url: 'https://sahathai-ui.vercel.app/logo/meta/meta-tag-projects.jpg',
+            width: 1800,
+            height: 1600,
+            alt: 'sahathai-meta-image-blog',
+          },
+        ],
+      },
+
+    }
+  }
+
+  const { metaTitle, metaDescription, metaImage } = body[0]
+
+  return {
+    title: metaTitle ?? 'Sahathai | blog',
+    description: metaDescription ?? '',
+    openGraph: {
+      title: metaTitle ?? 'Sahathai | blog',
+      description: metaDescription ?? '',
+      images: [
+        {
+          url: metaImage
+            ? metaImage.filename
+            : 'https://sahathai-ui.vercel.app/seo/meta-image-home.jpg',
+          width: 800,
+          height: 600,
+          alt: metaTitle ? metaTitle : 'sahathai-meta-image-blog',
+        },
+        {
+          url: metaImage
+            ? metaImage.filename
+            : 'https://sahathai-ui.vercel.app/' +
+              '/logo/meta/meta-tag-projects.jpg',
+          width: 1800,
+          height: 1600,
+          alt: metaTitle ? metaTitle : 'sahathai-meta-image-blog',
+        },
+      ],
+    },
+
+  }
+}
---
+++ src/features/blog/components/NewsBlogList/NewsBlogList.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { NewsCard } from '@/components/NewsCard'
+import { Pagination } from '@/components/Pagination'
+import { REVALIDATE_TIME } from '@/config/environtment'
+
+import {
+  fetchNewsBlogListData,
+  fetchNewsBlogListLengthData,
+} from '@/libs/storyblok'
+import { useLocale } from 'next-intl'
+
+export const revalidate = REVALIDATE_TIME
+
+import React, { useEffect, useState } from 'react'
+
+export default function NewsBlogList() {
+  const [newsBlog, setNewsBlog] = useState<any>()
+  const [newsBlogLength, setNewsBLogLength] = useState<number>(1)
+  const locale = useLocale()
+  const PER_PAGE = 9
+  useEffect(() => {
+    fetchNewsBlogListLengthData(1, 99, locale).then(({ data }) => {
+      setNewsBLogLength(data.stories.length)
+    })
+    fetchNewsBlogListData(1, PER_PAGE, locale).then(({ data }) => {
+      setNewsBlog(data)
+    })
+  }, [locale])
+
+  return (
+    <section className='flex flex-col justify-center items-center'>
+      <section className='max-w-[1100px] flex flex-wrap px-5 gap-5 mx-auto mb-10 flex-col md:flex-row justify-center items-center'>
+        {newsBlog &&
+          newsBlog.stories.map((newsBlog: any) => (
+            <NewsCard
+              key={newsBlog._uid}
+              title={newsBlog.content.body[0].newsTitle}
+              content={newsBlog.content}
+              createdAt={newsBlog.createdAt}
+              slug={newsBlog.slug}
+            />
+          ))}
+      </section>
+      <section>
+        {newsBlog && (
+          <Pagination
+            className='w-full news'
+            pageCount={newsBlogLength / PER_PAGE}
+            pageChange={v => {
+              fetchNewsBlogListData(v, PER_PAGE).then(({ data }) => {
+                setNewsBlog(data)
+              })
+            }}
+          />
+        )}
+      </section>
+    </section>
+  )
+}
