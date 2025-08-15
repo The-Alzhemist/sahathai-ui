1c9f5b42135bb599bc93d40ad252baf919ff7a45
Aphiwit1 <aphiwit@codework-tech.com>
2025-08-15 17:18:04 +0700

    fix(Loading): loading component img

--- src/components/Loading/LoadingUI.tsx
+++ src/components/Loading/LoadingUI.tsx
@@ -1,23 +1,20 @@
+import Image from 'next/image'
 
- export  const LoadingUi = () => {
+export  const LoadingUi = () => {
     return <div className="flex flex-col items-center justify-center min-h-screen bg-white text-white p-4">
       {/* Loading Spinner */}
-      <div className="relative w-20 h-20">
-        <div className="absolute top-0 left-0 w-full h-full border-4  rounded-full"></div>
-        <div className="absolute top-0 left-0 w-full h-full border-4 border-t-blue-300 rounded-full animate-spin"></div>
-      </div>
+
+
+      <Image src={'/time-2x.png'} alt={'loading image'} width={145} height={145} />
 
       {/* Loading Text */}
-      <h2 className="mt-8 text-2xl font-bold tracking-wide text-blue-300">
-        Loading...
+      <h2 className="mt-8 text-xl font-bold tracking-wide text-gray-800">
+        Loading
       </h2>
 
-      {/* Animated dots for a dynamic feel */}
-      <div className="flex space-x-2 mt-4">
-        <div className="w-2 h-2 bg-blue-300 rounded-full animate-bounce" style={{ animationDelay: '0.1s' }}></div>
-        <div className="w-2 h-2 bg-blue-300 rounded-full animate-bounce" style={{ animationDelay: '0.3s' }}></div>
-        <div className="w-2 h-2 bg-blue-300 rounded-full animate-bounce" style={{ animationDelay: '0.5s' }}></div>
-      </div>
+      <p className=' text-gray-800'>please wait...</p>
+
+
     </div>
  }
 
