94a8ff6259e5378cfaee95ae1b120107528842e9
Aphiwit1 <aphiwit@codework-tech.com>
2025-08-18 18:01:26 +0700

    fix(InvestorRelation): redesign

--- src/features/investorRelations/components/StockMarketInformation/StockMarketInformation.tsx
+++ src/features/investorRelations/components/StockMarketInformation/StockMarketInformation.tsx
@@ -6,6 +6,7 @@ import { InvestorInformationEnum } from '@/enums/investorRelations/InvestorInfor
 import { Animation } from '@/components/Animation'
 import { getTranslations } from 'next-intl/server'
 import { EODHD_STOCK_TOKEN, REVALIDATE_TIME } from '@/config/environtment'
+import Image from 'next/image'
 
 interface StockData {
   date: string
@@ -17,7 +18,7 @@ interface StockData {
   volume: number
 }
 
-export const revalidate = REVALIDATE_TIME
+export const revalidate = 3000
 
 const StockMarketInformation = async () => {
   // free stock api  update t - 3 day, get only 20 times/day
@@ -37,61 +38,65 @@ const StockMarketInformation = async () => {
     <Animation>
       <h2
         id={InvestorInformationEnum.StockMarketInformation}
-        className='headline-2 text-blue-400'
+        className='headline-2 text-navy text-center'
       >
         {t('title')}
       </h2>
 
-      <table className='w-full report shadow-6 rounded-[10px] overflow-hidden mt-[37px]'>
+      <table className='mx-auto max-w-[860px] w-full report shadow-7 rounded-[10px] overflow-hidden mt-[37px] '>
         <thead>
-          <tr>
-            <th className='subtitle-1 text-left'>ชื่อย่อหุ้น</th>
-            <th className='subtitle-1 text-right'>PORT</th>
+          <tr className='bg-white text-darkGray border-b px-5'>
+            <th className='subtitle-1 text-left !font-semibold '>ชื่อย่อหุ้น</th>
+            <th className='subtitle-1 text-right !font-semibold'>PORT</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className='px-6 py-4 text-left text-sm'>
-              <div className='flex flex-col'>
-                <span>ราคาสูงสุด</span>
-                <span className='text-xs text-gray-500'>
+        <tr className="border-b">
+          <td className="flex gap-5 items-center px-6 py-4 text-left text-sm">
+            <Image src={'/investor-relations/stock-up-2x.png'} alt={'stock up'} width={50} height={50}/>
+            <div className="flex flex-col">
+              <span>
+                ราคาสูงสุด</span>
+              <span className="text-xs text-gray-500">
                   (อัพเดทล่าสุดวันที่: {lastPriceData.date})
                 </span>
-              </div>
-            </td>
-            <td className='px-6 py-4 text-blue-400 text-right'>
-              {lastPriceData.high}
-            </td>
-          </tr>
-          <tr>
-            <td className='px-6 py-4 text-left text-sm'>
-              <div className='flex flex-col'>
-                <span>ราคาต่ำสุด</span>
-                <span className='text-xs text-gray-500'>
+            </div>
+          </td>
+          <td className="px-6 py-4 text-darkGray text-right">
+            {lastPriceData.high}
+          </td>
+        </tr>
+        <tr className="border-b">
+          <td className="flex gap-5 items-center px-6 py-4 text-left text-sm">
+            <Image src={'/investor-relations/stock-down-2x.png'} alt={'stock up'} width={50} height={50}/>
+            <div className="flex flex-col">
+              <span>ราคาต่ำสุด</span>
+              <span className="text-xs text-gray-500">
                   (อัพเดทล่าสุดวันที่: {lastPriceData.date})
                 </span>
-              </div>
-            </td>
-            <td className='px-6 py-4 text-blue-400 text-right'>
-              {lastPriceData.low}
-            </td>
-          </tr>
+            </div>
+          </td>
+          <td className="px-6 py-4 text-darkGray text-right">
+            {lastPriceData.low}
+          </td>
+        </tr>
 
-          <tr>
-            <td className='px-6 py-4 text-left text-sm'>เปลี่ยนแปลง</td>
-            <td className='px-6 py-4 text-blue-400 text-right'>
-              {`${changedPrice.toFixed(2)}`}
-            </td>
-          </tr>
+        <tr className="border-b">
 
-          <tr>
-            <td className='px-6 py-4 text-left text-sm'>
-              ปริมาณซื้อขาย (หุ้น)
-            </td>
-            <td className='px-6 py-4 text-blue-400 text-right'>
-              {lastPriceData.volume}
-            </td>
-          </tr>
+          <td className="flex gap-5 items-center px-6 py-4 text-left text-sm">         <Image src={'/investor-relations/stock-reverse-2x.png'} alt={'stock up'} width={50} height={50}/> เปลี่ยนแปลง</td>
+          <td className="px-6 py-4 text-darkGray text-right">
+            {`${changedPrice.toFixed(2)}`}
+          </td>
+        </tr>
+
+        <tr className="border-b">
+          <td className="px-6 py-4 text-left text-sm flex items-center gap-5">
+            <Image src={'/investor-relations/stock-graph-2x.png'} alt={'stock up'} width={50} height={50}/>  ปริมาณซื้อขาย (หุ้น)
+          </td>
+          <td className="px-6 py-4 text-darkGray text-right">
+            {lastPriceData.volume}
+          </td>
+        </tr>
         </tbody>
       </table>
     </Animation>
