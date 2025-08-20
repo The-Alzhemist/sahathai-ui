1eabd19613e934f3003131a8bc70630a58d60325
Aphiwit1 <aphiwit@codework-tech.com>
2025-08-20 09:06:57 +0700

    Update storyblok.ts

--- src/utils/storyblok.ts
+++ src/utils/storyblok.ts
@@ -3,7 +3,7 @@ import { STORYBLOK_BASE_URL, STORYBLOK_TOKEN } from '@/config/environtment'
 export async function fetchStoryblokStory(slug: string, locale: string, version: 'draft' | 'published' = 'draft') {
   const url = `${STORYBLOK_BASE_URL}/stories/${slug}?version=${version}&token=${STORYBLOK_TOKEN}&language=${locale}`
 
-  const res = await fetch(url, { next: { revalidate: 300 } })
+  const res = await fetch(url, { next: { revalidate: 60 } })
   if (!res.ok) {
     throw new Error(`Storyblok fetch failed: ${res.status}`)
   }
