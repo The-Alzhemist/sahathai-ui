86ba1934c0a5ee282ff568ff3f032e0db69317d8
Aphiwit1 <aphiwit@codework-tech.com>
2025-08-20 15:05:52 +0700

    fix(InvestorRelation): redesign video and factSheet section

--- src/features/investorRelations/pages/InvestorInformationPage/InvestorInformationPage.tsx
+++ src/features/investorRelations/pages/InvestorInformationPage/InvestorInformationPage.tsx
@@ -13,6 +13,9 @@ import { Banner } from '@/components/Banner'
 import { getTranslations } from 'next-intl/server'
 import { CompanyAdvertiseVideo } from '@/features/investorRelations/components/CompanyAdvertiseVideo'
 import { CompanyFactSheet } from '@/features/investorRelations/components/CompanyFactSheet'
+import { Background } from '@/components/Background'
+import { DoubleQuoteIcon } from '@/components/icons/DoubleQuoteIcon'
+import Image from 'next/image'
 
 export async function InvestorInformationPage() {
   const t = await getTranslations('Menu')
@@ -26,21 +29,42 @@ export async function InvestorInformationPage() {
         className='mb-[40px]'
       />
       {/*<Tabs activeMenu={InvestorRelationEnum.InvestorInformation} />*/}
-      <section className=' w-full  mx-auto  mt-[80px] '>
+      <section className=' w-full  mx-auto   '>
         <div className='hidden md:flex'>
           {/*<SideMenu />*/}
         </div>
 
-        <section className='w-full space-y-[112px]'>
-          <StockMarketInformation />
+        <section className='w-full space-y-[112px] '>
+
+          {/*<StockMarketInformation />*/}
           <FinancialReports />
           {/*<Shareholder />*/}
           {/*<DividendPayment className=' w-full' />*/}
           {/*<ShareholdersMeeting />*/}
 
-          <CompanyIntroductionVideo />
-          <CompanyAdvertiseVideo/>
-          <CompanyFactSheet/>
+
+          <Background className='!my-0'>
+          <div className='flex flex-col gap-y-10 bg-modellBgDark/60 py-[100px] px-5'>
+            <CompanyIntroductionVideo />
+            <DoubleQuoteIcon
+              className='text-secondary self-center'
+              width='36'
+              height='17'
+            />
+            <CompanyAdvertiseVideo/>
+          </div>
+          </Background>
+
+          <div className='w-full relative px-5 !mt-0'>
+            <div
+              className="absolute opacity-80 inset-0 bottom-0 z-0 bg-[url('/investor-relations/factsheet-bg.webp')] bg-center bg-no-repeat bg-cover"
+            />
+
+            <div className='max-w-[860px]  mx-auto py-[100px] '>
+              <CompanyFactSheet />
+            </div>
+
+          </div>
         </section>
       </section>
     </main>
