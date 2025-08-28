72014748c20c8ea508b5f51c1e529ad172978ed8
Aphiwit1 <aphiwit@codework-tech.com>
2025-08-28 15:57:41 +0700

    feat(Blog): pagination and fetchLastBlog

--- src/features/blog/pages/NewsPage/withBlogPage.tsx
+++
@@ -1,9 +0,0 @@
-import { FunctionComponentType } from '@/models/FunctionComponentType'
-
-export function withBlogPage(Component: React.FC<FunctionComponentType>) {
-  function WithBlogPage() {
-    return <Component />
-  }
-
-  return WithBlogPage
-}
