6f0e214e87fe09c91f7139f84afb97cee64b484b
Aphiwit1 <aphiwit@codework-tech.com>
2025-09-01 13:09:30 +0700

    fix(revalidate): refactor revalidate tag

--- src/utils/storyblok.ts
+++ src/utils/storyblok.ts
@@ -3,12 +3,9 @@ import { STORYBLOK_BASE_URL, STORYBLOK_TOKEN } from '@/config/environtment'
 export async function fetchStoryblokStory(slug: string, locale: string, version: 'draft' | 'published' = 'draft',revalidate = 300) {
   const url = `${STORYBLOK_BASE_URL}/stories/${slug}?version=${version}&token=${STORYBLOK_TOKEN}&language=${locale}`
 
-  // ใส่ tag อย่างน้อย 2 ระดับ: ต่อเรื่อง และ ต่อเพจ/เส้นทาง
-  const tags = [
-    `story:${slug}`,                 // เจาะจงเรื่อง
-    `story:${locale}:${slug}`,       // เจาะจงเรื่อง+ภาษา
-    'story:investor-accordion-list' // list/global section (ถ้ามี)
-  ]
+
+  const tags = ['story:investor-accordion-list']
+
 
   const res = await fetch(url, { next: { revalidate, tags } })
 
