ec8ba1b3c9c7254a2453acd28b52dd738fb70ad5
Aphiwit1 <aphiwit@codework-tech.com>
2025-09-11 12:34:02 +0700

    feat(investor): meta

--- src/app/[locale]/investor-information/share-holder/page.tsx
+++ src/app/[locale]/investor-information/share-holder/page.tsx
@@ -1,8 +1,23 @@
 
 import { ShareHolderPage } from '@/features/investorRelations/pages/ShareHolderPage'
+import { getTranslations } from 'next-intl/server'
 
 
 
 export default async function ShareHolder() {
   return <ShareHolderPage />
+}
+
+
+export async function generateMetadata() {
+  const t = await getTranslations('MetaData')
+
+  return {
+    title: t('PerformanceReport.Title'),
+    description: t('PerformanceReport.Description'),
+    openGraph: {
+      title: t('PerformanceReport.Title'),
+      description: t('PerformanceReport.Description'),
+    },
+  }
 }
