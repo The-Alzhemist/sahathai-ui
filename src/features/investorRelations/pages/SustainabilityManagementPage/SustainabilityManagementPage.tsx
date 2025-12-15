1865ab4ac6e45adbcebc33dfab2d4dfb86fdc86a
Aphiwit1 <aphiwit@codework-tech.com>
2025-12-15 13:53:26 +0700

    feat(InvestorRelation): change all image

--- src/features/investorRelations/pages/SustainabilityManagementPage/SustainabilityManagementPage.tsx
+++ src/features/investorRelations/pages/SustainabilityManagementPage/SustainabilityManagementPage.tsx
@@ -7,14 +7,10 @@ import { Animation } from '@/components/Animation'
 import { Menu } from '@/components/Menu'
 import { Banner } from '@/components/Banner'
 
-
 import { DoubleQuoteIcon } from '@/components/icons/DoubleQuoteIcon'
 import { EsgRisk } from '@/features/investorRelations/pages/SustainabilityManagementPage/components/EsgRisk/EsgRisk'
-import {
-  EmergingRisk
-} from '@/features/investorRelations/pages/SustainabilityManagementPage/components/EmergingRisk/EmergingRisk'
+import { EmergingRisk } from '@/features/investorRelations/pages/SustainabilityManagementPage/components/EmergingRisk/EmergingRisk'
 import ESGPolicy from '@/features/investorRelations/pages/SustainabilityManagementPage/components/ESGPolicy/ESGPolicy'
-
 
 export function SustainabilityManagementPage() {
   const t = useTranslations('SustainabilityManagementPage')
@@ -22,56 +18,54 @@ export function SustainabilityManagementPage() {
     <main className=''>
       <Menu />
       <Banner
-        imagePath='/about-us/banner.png'
+        imagePath='/home/new/sahathai-building-banner.webp'
         alt={t('investorRelations')}
         caption={t('title')}
         className=''
       />
       {/*<Tabs activeMenu={InvestorRelationEnum.SustainabilityManagement} />*/}
-      <section className="relative pt-[20px] pb-[30px] overflow-hidden">
+      <section className='relative pt-[-20px] pb-[30px] overflow-hidden'>
         {/* background image */}
         <Image
-          src="/investor-relations/esg-section-1-bg.webp"
-          alt="Sustainability Background"
+          src='/investor-relations/new/ir-sustain-bg.webp'
+          alt='Sustainability Background'
           fill
-          className="absolute inset-0 object-cover object-center z-0"
+          className='absolute inset-0 object-cover object-center z-0'
           priority
         />
 
         {/* content */}
-        <div className="relative z-10 max-w-[950px] mx-auto px-5">
-          <h2
-            className="flex items-center justify-center md:justify-start gap-2 mt-[103px] headline-2 text-left text-blue-400">
-            <DoubleQuoteIcon className="text-red-400" /> {t('title')}
+        <div className='relative z-10 max-w-[950px] mx-auto px-5'>
+          <h2 className='flex items-center justify-center md:justify-start gap-2 mt-[70px] headline-2 text-left text-blue-400'>
+            <DoubleQuoteIcon className='text-red-400' /> {t('title')}
           </h2>
         </div>
 
-        <Animation
-          className="relative z-10 mt-[50px] mb-[100px] max-w-[950px] mx-auto flex gap-y-5 justify-center md:justify-between flex-wrap">
+        <Animation className='relative z-10 mt-[50px] mb-[100px] max-w-[950px] mx-auto flex gap-y-5 justify-center md:justify-between flex-wrap'>
           <SustainabilityCard
             title={t('economicDimension.title')}
             descriptions={[t('economicDimension.description')]}
-            imagePath="/investor-relations/hand-red.png"
-            imgClassName="w-[70px] h-[49px]"
+            imagePath='/investor-relations/hand-red.png'
+            imgClassName='w-[70px] h-[49px]'
           />
           <SustainabilityCard
             title={t('socialDimension.title')}
             descriptions={[
               t('socialDimension.description'),
               t('socialDimension.description2'),
-              t('socialDimension.description3')
+              t('socialDimension.description3'),
             ]}
-            imagePath="/investor-relations/people-red.png"
-            imgClassName="w-[80px] h-[60px]"
+            imagePath='/investor-relations/people-red.png'
+            imgClassName='w-[80px] h-[60px]'
           />
           <SustainabilityCard
             title={t('environmentalDimension.title')}
             descriptions={[
               t('environmentalDimension.description'),
               t('environmentalDimension.description2'),
-              t('environmentalDimension.description3')
+              t('environmentalDimension.description3'),
             ]}
-            imagePath="/investor-relations/heart-red.png"
+            imagePath='/investor-relations/heart-red.png'
           />
         </Animation>
       </section>
@@ -80,14 +74,10 @@ export function SustainabilityManagementPage() {
       <EmergingRisk />
       <ESGPolicy />
 
-      <section className="space-y-[40px] py-[88px] bg-white">
+      <section className='space-y-[40px] py-[88px] bg-white'>
         <Animation className='max-w-[960px] w-full mx-auto'>
           <div className='relative pt-[86.88%] w-full '>
-            <Image
-              src='/investor-relations/sustainable-1.png'
-              alt=''
-              fill
-            />
+            <Image src='/investor-relations/sustainable-1.png' alt='' fill />
           </div>
         </Animation>
         <Animation className='max-w-[969px] w-full mx-auto'>
