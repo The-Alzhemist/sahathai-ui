28ed3b62ee7280d9d45b441afe565f31d7d5de97
Aphiwit1 <aphiwit@codework-tech.com>
2025-12-15 13:53:26 +0700

    feat(InvestorRelation): change all image

--- src/features/investorRelations/pages/InvestorInformationPage/InvestorInformationPage.tsx
+++ src/features/investorRelations/pages/InvestorInformationPage/InvestorInformationPage.tsx
@@ -1,4 +1,3 @@
-
 import { FinancialReports } from '../../components/FinancialReports'
 
 import { CompanyIntroductionVideo } from '../../components/CompanyIntroductionVideo'
@@ -12,49 +11,40 @@ import { CompanyFactSheet } from '@/features/investorRelations/components/Compan
 import { Background } from '@/components/Background'
 import { DoubleQuoteIcon } from '@/components/icons/DoubleQuoteIcon'
 
-
-
 export async function InvestorInformationPage() {
   const t = await getTranslations('Menu')
   return (
     <main className='bg-[#F5f5f5]'>
       <Menu />
       <Banner
-        imagePath='/about-us/banner.png'
+        imagePath='/home/new/sahathai-building-banner.webp'
         alt={t('investorRelations.title')}
         caption={t('investorRelations.title')}
         className='mb-[40px]'
       />
       <section className=' w-full  mx-auto   '>
-
-
         <section className='w-full space-y-[112px] '>
-
           <StockMarketInformation />
           <FinancialReports />
 
-
           <Background className='!my-0'>
-          <div className='flex flex-col gap-y-10 bg-modellBgDark/60 py-[100px] px-5'>
-            <CompanyIntroductionVideo />
-            <DoubleQuoteIcon
-              className='text-secondary self-center'
-              width='36'
-              height='17'
-            />
-            <CompanyAdvertiseVideo/>
-          </div>
+            <div className='flex flex-col gap-y-10 bg-modellBgDark/60 py-[100px] px-5'>
+              <CompanyIntroductionVideo />
+              <DoubleQuoteIcon
+                className='text-secondary self-center'
+                width='36'
+                height='17'
+              />
+              <CompanyAdvertiseVideo />
+            </div>
           </Background>
 
           <div className='w-full relative px-5 !mt-0'>
-            <div
-              className="absolute opacity-80 inset-0 bottom-0 z-0 bg-[url('/investor-relations/factsheet-bg.webp')] bg-center bg-no-repeat bg-cover"
-            />
+            <div className="absolute opacity-100 inset-0 bottom-0 z-0 bg-[url('/investor-relations/new/investor-relation-contact-bg.webp')] bg-center bg-no-repeat bg-cover" />
 
             <div className='max-w-[860px]  mx-auto py-[100px] '>
               <CompanyFactSheet />
             </div>
-
           </div>
         </section>
       </section>
