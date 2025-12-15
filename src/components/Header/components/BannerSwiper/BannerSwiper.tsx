6584148201be06988397d3d350f4ffcb71cf24bd
Aphiwit1 <aphiwit@codework-tech.com>
2025-12-15 17:53:25 +0700

    feat(HomePage): test video 22 mb

--- src/components/Header/components/BannerSwiper/BannerSwiper.tsx
+++ src/components/Header/components/BannerSwiper/BannerSwiper.tsx
@@ -9,11 +9,19 @@ import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
+type Slide =
+  | { type: 'image'; src: string }
+  | { type: 'video'; src: string; poster?: string }
+
 export default function SwiperVertical() {
-  const slides = [
-    '/home/new/sahathai-building-banner.webp',
-    '/home/new/sahathai-building-banner-2.webp',
-    '/home/new/sahathai-building-banner-3.webp',
+  const slides: Slide[] = [
+    {
+      type: 'video',
+      src: '/home/new/movie.mp4',
+    },
+    { type: 'image', src: '/home/new/sahathai-building-banner.webp' },
+
+    { type: 'image', src: '/home/new/sahathai-building-banner-2.webp' },
   ]
 
   const swiperConfig: SwiperProps = {
@@ -21,9 +29,9 @@ export default function SwiperVertical() {
     direction: 'vertical',
     slidesPerView: 1,
     loop: true,
-    autoplay: { delay: 10000 },
+    autoplay: { delay: 1000099 },
     watchSlidesProgress: true,
-    className: 'mySwiper w-full h-[480px]',
+    className: 'mySwiper w-full h-[825px]',
     navigation: {
       prevEl: '.custom-prev',
       nextEl: '.custom-next',
@@ -35,43 +43,47 @@ export default function SwiperVertical() {
   }
 
   return (
-    <section className='relative w-full h-[480px]'>
+    <section className='relative w-full h-[825px]'>
       <Swiper {...swiperConfig}>
         {slides.map((slide, index) => (
           <SwiperSlide key={`banner-slide-${index}`}>
-            <div className='relative w-full h-[480px]'>
-              <Image
-                src={slide}
-                alt={`Slide ${index + 1}`}
-                fill
-                className='object-cover md:rounded-b-[60px]'
-                loading='eager'
-                unoptimized
-                priority={index === 0}
-              />
+            <div className='relative w-full h-[825px]'>
+              {slide.type === 'image' && (
+                <Image
+                  src={slide.src}
+                  alt={`Slide ${index + 1}`}
+                  fill
+                  className='object-cover md:rounded-b-[60px]'
+                  priority={index === 0}
+                />
+              )}
+
+              {slide.type === 'video' && (
+                <>
+                  <div className='absolute inset-0 w-full h-full bg-transparent' />
+                  <video
+                    className='w-full h-full object-cover md:rounded-b-[60px]'
+                    autoPlay
+                    loop
+                    muted
+                  >
+                    {/* <source
+                      src='/home/new/sahathai-video-480p.mp4'
+                      type='video/mp4'
+                    /> */}
+                    <source
+                      src='/home/new/sahathai-video-480p.mp4'
+                      type='video/mp4'
+                    />
+                    Your browser does not support HTML video.
+                  </video>
+                </>
+              )}
             </div>
           </SwiperSlide>
         ))}
       </Swiper>
 
-      {/* Custom Navigation Buttons */}
-      {/*<div*/}
-      {/*  className="absolute*/}
-      {/*       right-4 top-1/2 -translate-y-1/2 flex flex-col items-center gap-y-5*/}
-      {/*       md:right-10 md:top-auto md:bottom-4 md:-translate-y-0 md:gap-y-2 z-50">*/}
-      {/*  <button*/}
-      {/*    className="custom-prev border-2 border-red-400 text-red-400 bg-red-100*/}
-      {/*         rounded-full h-[30px] w-[30px] flex items-center justify-center">*/}
-      {/*    ↑*/}
-      {/*  </button>*/}
-      {/*  <button*/}
-      {/*    className="custom-next border-2 border-red-400 text-red-400 bg-red-100*/}
-      {/*         rounded-full h-[30px] w-[30px] flex items-center justify-center">*/}
-      {/*    ↓*/}
-      {/*  </button>*/}
-      {/*</div>*/}
-
-      {/* 🔹 Custom Pagination Center Right */}
       <div
         className='custom-pagination absolute right-2 top-1/2 -translate-y-1/2
                    flex flex-col items-center gap-0.5 !w-auto z-50'
@@ -79,3 +91,13 @@ export default function SwiperVertical() {
     </section>
   )
 }
+
+// <video
+//                 className='w-full h-full object-cover md:rounded-b-[60px]'
+//                 muted
+//                 playsInline
+//                 preload='metadata'
+//                 poster={slide.poster}
+//               >
+//                 <source src={slide.src} type='video/mp4' />
+//               </video>
