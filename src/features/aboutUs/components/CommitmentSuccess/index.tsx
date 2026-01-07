377b506584f63236f6701a2066327362a8495c45
Aphiwit1 <aphiwit@codework-tech.com>
2026-01-07 08:26:07 +0700

    fix(Image):

--- src/features/aboutUs/components/CommitmentSuccess/index.tsx
+++ src/features/aboutUs/components/CommitmentSuccess/index.tsx
@@ -8,7 +8,7 @@ export function CommitmentSuccess() {
   const t = useTranslations('AboutUsPage.CommitmentSuccess')
 
   return (
-    <section className='mt-[80px] pb-[300px] w-full relative'>
+    <section className='pt-[80px] pb-[300px] w-full relative'>
       <div className=' max-w-[1000px] w-full mx-auto px-5'>
         <SahathaiText h='h1' className='headline-3 md:headline-1' />
         <h3 className='mt-[24px] headline-4'>{t('startingBusiness')}</h3>
@@ -17,12 +17,17 @@ export function CommitmentSuccess() {
         </p>
         <Timeline />
       </div>
-      <div className='absolute -z-10 bottom-0 left-0 h-[704px] w-full'>
+      <div className='absolute -z-10 bottom-0 left-0 h-full w-full'>
         <Image
-          src='/about-us/new/commitment-bg.webp'
+          src='/about-us/new/commitment-bg-2.webp'
           alt='About us background'
           fill
-          className='object-bottom'
+          className='
+      object-cover
+      object-center
+      md:object-bottom
+    '
+          priority
         />
       </div>
     </section>
