4b9c1dfce822b267c7e7c71700d095e91f8fb8fb
Aphiwit1 <aphiwit@codework-tech.com>
2026-01-13 14:35:23 +0700

    fix(investor): annual report document

--- src/features/investorRelations/components/OneReportTable/oneReportInformationList.tsx
+++ src/features/investorRelations/components/OneReportTable/oneReportInformationList.tsx
@@ -1,38 +1,62 @@
 export const oneReportInformationList = [
   {
-    year: 2023,
-    annualReportUrl:
-      '',
-    report56Url:
-      '',
+    year: 2024,
+    annualReportUrl: '',
+    report56Url: '',
     oneReportUrl:
-      'https://storage.googleapis.com/nueng-image-storage-demo/dummyPhoto/dummy.pdf',
+      'https://a.storyblok.com/f/316761/x/b6696f762b/one_report_2024.pdf',
+  },
+  {
+    year: 2023,
+    annualReportUrl: '',
+    report56Url: '',
+    oneReportUrl:
+      'https://a.storyblok.com/f/316761/x/fbd9d13e34/one_report_2023.pdf',
   },
   {
     year: 2022,
-    annualReportUrl:
-      '',
-    report56Url:
-      '',
+    annualReportUrl: '',
+    report56Url: '',
     oneReportUrl:
-      'https://storage.googleapis.com/nueng-image-storage-demo/dummyPhoto/dummy.pdf',
+      'https://a.storyblok.com/f/316761/x/f50697288e/one_report_2022.pdf',
   },
   {
     year: 2021,
-    annualReportUrl:
-      '',
-    report56Url:
-      '',
+    annualReportUrl: '',
+    report56Url: '',
     oneReportUrl:
-      'https://storage.googleapis.com/nueng-image-storage-demo/dummyPhoto/dummy.pdf',
+      'https://a.storyblok.com/f/316761/x/f198f0dea7/one_report_2021.pdf',
   },
   {
     year: 2020,
     annualReportUrl:
-      '',
+      'https://a.storyblok.com/f/316761/x/2d29e06be8/annual_report_2020.pdf',
     report56Url:
-      '',
-    oneReportUrl:
-      'https://storage.googleapis.com/nueng-image-storage-demo/dummyPhoto/dummy.pdf',
+      'https://a.storyblok.com/f/316761/x/f8f5f54700/report_56-1_2020.zip',
+    oneReportUrl: '',
+  },
+  {
+    year: 2019,
+    annualReportUrl:
+      'https://a.storyblok.com/f/316761/x/c3f41d2764/annual_report_2019.pdf',
+    report56Url:
+      'https://a.storyblok.com/f/316761/x/ee46eedc5d/report56-1_2019.7z',
+    oneReportUrl: '',
+  },
+  {
+    year: 2018,
+    annualReportUrl:
+      'https://a.storyblok.com/f/316761/x/c3f41d2764/annual_report_2018.pdf',
+    report56Url:
+      'https://a.storyblok.com/f/316761/x/d90e8fafb5/report56-1_2018.zip',
+    oneReportUrl: '',
+  },
+  {
+    year: 2017,
+    annualReportUrl:
+      'https://a.storyblok.com/f/316761/x/1dda08ce1b/annual_report_2017.pdf',
+    report56Url:
+      'https://a.storyblok.com/f/316761/x/0eb9c46ac2/report_56-1_2017.zip',
+    oneReportUrl: '',
   },
 ]
