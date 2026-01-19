bc75b1c3f44ad40911350fbe517d26292506d66c
Aphiwit1 <aphiwit@codework-tech.com>
2026-01-19 16:49:29 +0700

    fix(Inverstor): title

--- src/features/investorRelations/pages/WarrantPage/WarrantPage.tsx
+++ src/features/investorRelations/pages/WarrantPage/WarrantPage.tsx
@@ -40,6 +40,9 @@ export function WarrantPage({ data }: WarrantPageProps) {
         alt='investor-banner-11'
       />
       <section className='px-5 pb-5  pt-[50px] md:pt-[100px] max-w-4xl mx-auto space-y-6'>
+        <h1 className='text-lg md:text-3xl mb-10 text-blue-400 text-center'>
+          {tMenu('investorRelations.Warrant')}
+        </h1>
         {group.map((groupItem: any, groupIndex: number) => (
           <div key={groupIndex} className=' rounded-md p-4'>
             {/* --- Group Header (always visible) --- */}
