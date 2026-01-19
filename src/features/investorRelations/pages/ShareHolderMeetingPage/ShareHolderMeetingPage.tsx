155772ce0d79be31a03471313f0c462f45f65836
Aphiwit1 <aphiwit@codework-tech.com>
2026-01-19 16:49:29 +0700

    fix(Inverstor): title

--- src/features/investorRelations/pages/ShareHolderMeetingPage/ShareHolderMeetingPage.tsx
+++ src/features/investorRelations/pages/ShareHolderMeetingPage/ShareHolderMeetingPage.tsx
@@ -42,6 +42,9 @@ export function ShareHolderMeetingPage({
       />
 
       <section className='px-5 pb-5  pt-[50px] md:pt-[100px] max-w-4xl mx-auto space-y-6'>
+        <h1 className='text-lg md:text-3xl mb-7 text-blue-400 text-center'>
+          {tMenu('investorRelations.shareHolderMeeting')}
+        </h1>
         {group.map((groupItem: any, groupIndex: number) => (
           <div key={groupIndex} className=' rounded-md p-4'>
             {/* --- Group Header (always visible) --- */}
