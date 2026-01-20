f27d65e37be9514d67e5b6a8c9b22779238ed633
Aphiwit1 <aphiwit@codework-tech.com>
2026-01-20 21:05:25 +0700

    fix(Blog)

--- src/components/BlogCard/index.tsx
+++ src/components/BlogCard/index.tsx
@@ -7,13 +7,13 @@ import { extractTextFieldsStoryblok } from '@/utils/extractTextFieldsStoryblok'
 import type { NewsCardProps } from './interface'
 
 export function BlogCard({
-                           title,
-                           content,
-                           createdAt,
-                           slug,
-                           direction = 'vertical',
-  page = 'news'
-                         }: NewsCardProps) {
+  title,
+  content,
+  createdAt,
+  slug,
+  direction = 'vertical',
+  page = 'news',
+}: NewsCardProps) {
   const common = useTranslations('common')
 
   const body0 = content?.body?.[0]
@@ -27,33 +27,31 @@ export function BlogCard({
         'block w-full md:w-[290px] overflow-hidden rounded-[8px] p-2  hover:scale-[1.02] hover:shadow-1 transition-all  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2'
       )}
     >
-      <article className="w-full">
+      <article className='w-full'>
         <Image
           src={imageSrc}
           alt={title}
           width={500}
           height={500}
-          className="w-full h-[250px] md:h-[180px] object-cover rounded-[8px]"
+          className='w-full h-[250px] md:h-[180px] object-cover rounded-[8px]'
         />
 
-        <h2 className="mt-[16px] headline-4 line-clamp-2 text-black">
+        <h2 className='mt-[16px] headline-4 line-clamp-2 text-black h-[60px]'>
           {title}
         </h2>
 
-        <p className="mt-[8px] body-2 line-clamp-2 text-black-6">
+        <p className='mt-[8px] body-2 line-clamp-2 text-black-6 h-[42px]'>
           {extractTextFieldsStoryblok(content)}
         </p>
 
-        <div className="mt-[8px] caption text-black-3">
-          {newsDate}
-        </div>
+        <div className='mt-[8px] caption text-black-3'>{newsDate}</div>
 
-        <div className="mt-[16px] button-small text-navy w-fit flex gap-[10px] items-center">
+        <div className='mt-[16px] button-small text-navy w-fit flex gap-[10px] items-center'>
           <span
             aria-hidden
-            className="p-[10px] bg-navy rounded-full text-white inline-flex items-center justify-center"
+            className='p-[10px] bg-navy rounded-full text-white inline-flex items-center justify-center'
           >
-            <ArrowRightIcon width="10" height="10" />
+            <ArrowRightIcon width='10' height='10' />
           </span>
           {common('readMore')}
         </div>
