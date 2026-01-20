09fdcba4627e8b22ef43a2e83ff5a386c52351c9
Aphiwit1 <aphiwit@codework-tech.com>
2026-01-20 17:24:45 +0700

    fix(layout): image #2

--- src/features/investorRelations/components/SustainabilityCard/index.tsx
+++ src/features/investorRelations/components/SustainabilityCard/index.tsx
@@ -5,20 +5,23 @@ export function SustainabilityCard({
   title,
   descriptions,
   imagePath,
-  imgClassName
+  imgClassName,
 }: SustainabilityCardProps) {
   return (
     <div className='max-w-[300px] w-full rounded-[20px] p-[16px] shadow-2 bg-white'>
       {imagePath && (
         <div className='flex justify-center mb-5 h-[60px]'>
-          <Image  src={imagePath} alt={imagePath} width={70} height={70} className={imgClassName}/>
+          <Image
+            src={imagePath}
+            alt={imagePath}
+            width={70}
+            height={70}
+            className={imgClassName}
+          />
         </div>
-
       )}
 
-      <h2 className='headline-4 text-navy text-center mb-[24px]'>
-        {title}
-      </h2>
+      <h2 className='headline-4 text-navy text-center mb-3'>{title}</h2>
       {descriptions.map((description, index) => (
         <div
           className='grid grid-cols-[10px_1fr] body-1  text-black-60 whitespace-pre-line'
