908343bfec306262c40e3e1c8942f544da476f60
Aphiwit1 <aphiwit@codework-tech.com>
2026-01-22 12:27:16 +0700

    feat(Eservice): storyblok #1

--- src/features/investorRelations/pages/EServicePage/withEServicePage.tsx
+++ src/features/investorRelations/pages/EServicePage/withEServicePage.tsx
@@ -1,8 +1,9 @@
+import { EServicePageProps } from '@/app/[locale]/e-service/interface'
 import { FunctionComponentType } from '@/models/FunctionComponentType'
 
-export function withEServicePage(Component: React.FC<FunctionComponentType>) {
-  function WithEServicePage() {
-    return <Component />
+export function withEServicePage(Component: React.FC<EServicePageProps>) {
+  function WithEServicePage(props: EServicePageProps) {
+    return <Component {...props} />
   }
 
   return WithEServicePage
