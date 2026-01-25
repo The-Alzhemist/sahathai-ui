aaa86ab88fee9f689143cb980bdc591ac86802f9
Aphiwit1 <aphiwit@codework-tech.com>
2026-01-25 09:30:45 +0700

    fix(Banner): ui banner popup

---
+++ src/components/BannerPopup/component/BannerPopupContent.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
+
+export default function BannerPopupContent({ data }: any) {
+  const [open, setOpen] = useState(true)
+
+  const isShowBannerConfig = data.story.content.body[0].isShowBanner
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  if (!open || !isShowBannerConfig) {
+    return null
+  }
+
+  return (
+    <section className='fixed inset-0 z-50 flex items-center justify-center bg-black/60 p-4'>
+      <div className='relative w-fit max-w-[800px] h-fit'>
+        <Image
+          src={data.story.content.body[0].bannerImage.filename}
+          alt={'Banner'}
+          width={800}
+          height={800}
+          className='block w-auto h-auto max-w-full max-h-[80vh] object-contain rounded-lg shadow-2xl'
+        />
+
+        <div
+          onClick={handleClose}
+          className='absolute top-3 right-3 z-10 
+                 flex items-center justify-center
+                 w-8 h-8 rounded-full 
+                 bg-black/60 hover:bg-black/80 text-white 
+                 cursor-pointer transition-all shadow-md'
+        >
+          <span className='text-xl leading-none'>✕</span>
+        </div>
+      </div>
+    </section>
+  )
+}
