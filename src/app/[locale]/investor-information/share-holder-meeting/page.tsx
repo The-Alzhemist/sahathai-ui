070140c34dda27f7200ba86346701bdf7d9a1db7
Aphiwit1 <aphiwit@codework-tech.com>
2026-01-26 12:14:57 +0700

    fi(storyblok): investor-path

--- src/app/[locale]/investor-information/share-holder-meeting/page.tsx
+++ src/app/[locale]/investor-information/share-holder-meeting/page.tsx
@@ -1,28 +1,22 @@
-import {
-  ShareHolderMeetingPage,
-} from '@/features/investorRelations/pages/ShareHolderMeetingPage/ShareHolderMeetingPage'
+import { ShareHolderMeetingPage } from '@/features/investorRelations/pages/ShareHolderMeetingPage/ShareHolderMeetingPage'
 import { fetchStoryblokStory } from '@/libs/storyblok/accordionsQuery'
 import { getTranslations } from 'next-intl/server'
 
-
-
 export default async function ShareHolderMeeting({
-                                                   params,
-                                                 }: {
+  params,
+}: {
   params: { locale: string }
 }) {
   const { locale } = params
 
   const response = await fetchStoryblokStory(
-    'invrester-relartion/90ee2b39-0565-41ce-bdb4-dad2425245a4',
+    'investor-relations/90ee2b39-0565-41ce-bdb4-dad2425245a4',
     locale,
     'published'
   )
 
-
   return <ShareHolderMeetingPage shareHolderMeetingData={response} />
 }
-
 
 export async function generateMetadata() {
   const t = await getTranslations('MetaData')
