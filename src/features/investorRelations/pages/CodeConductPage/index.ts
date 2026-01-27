5ec3543cfceb91b319f696c170b35509b3b66370
Aphiwit1 <aphiwit@codework-tech.com>
2026-01-27 15:15:05 +0700

    feat(CodeOfConduct): change code and UI to support storyblok

--- src/features/investorRelations/pages/CodeConductPage/index.ts
+++ src/features/investorRelations/pages/CodeConductPage/index.ts
@@ -1,6 +1,3 @@
-import { CodeConductPage } from './CodeConductPage'
-import { withCodeConductPage } from './withCodeConductPage'
-
-const ConnectedCodeConductPage = withCodeConductPage(CodeConductPage)
-
-export { ConnectedCodeConductPage as CodeConductPage }
+export interface CodeOfConductPageProps {
+  conOdConductData: any
+}
