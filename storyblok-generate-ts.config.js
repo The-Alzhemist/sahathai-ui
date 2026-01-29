49618c8bc48ba4892bb98cd78f6ccdc49fa52953
Aphiwit1 <aphiwit@codework-tech.com>
2026-01-29 08:26:15 +0700

    feat(storyblok): generaqte type command

---
+++ storyblok-generate-ts.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // ✅ ต้องใช้ key นี้เท่านั้น
+  componentsJsonPath: '.storyblok/components/316761/components.json',
+
+  // output types
+  outputFile: 'src/types/storyblok.ts',
+}
