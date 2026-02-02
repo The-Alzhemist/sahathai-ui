002f3b29dfa19ae00e354cbb0d24baee0c244c95
Aphiwit1 <aphiwit@codework-tech.com>
2026-02-02 15:32:48 +0700

    fix(Sustainability): image for 3 langs

--- src/features/investorRelations/pages/SustainabilityManagementPage/SustainabilityManagementPage.tsx
+++ src/features/investorRelations/pages/SustainabilityManagementPage/SustainabilityManagementPage.tsx
@@ -1,21 +1,21 @@
-import { useTranslations } from 'next-intl'
+import { useLocale, useTranslations } from 'next-intl'
 import Image from 'next/image'
 
 import { SustainabilityCard } from '../../components/SustainabilityCard'
 
 import { Animation } from '@/components/Animation'
 import { Menu } from '@/components/Menu'
-import { Banner } from '@/components/Banner'
 
 import { DoubleQuoteIcon } from '@/components/icons/DoubleQuoteIcon'
 import { EsgRisk } from '@/features/investorRelations/pages/SustainabilityManagementPage/components/EsgRisk/EsgRisk'
 import { EmergingRisk } from '@/features/investorRelations/pages/SustainabilityManagementPage/components/EmergingRisk/EmergingRisk'
 import ESGPolicy from '@/features/investorRelations/pages/SustainabilityManagementPage/components/ESGPolicy/ESGPolicy'
-import SwiperVertical from '@/components/Header/components/BannerSwiper/BannerSwiper'
+
 import BannerImage from '@/components/Header/components/BannerImage/BannerImage'
 
 export function SustainabilityManagementPage() {
   const t = useTranslations('SustainabilityManagementPage')
+  const locale = useLocale()
   return (
     <main className=''>
       <Menu />
@@ -25,9 +25,7 @@ export function SustainabilityManagementPage() {
         imageSrc='/investor-relations/new/investor-banner-2.webp'
         alt='investor-banner-2'
       />
-      {/*<Tabs activeMenu={InvestorRelationEnum.SustainabilityManagement} />*/}
       <section className='relative pt-[-20px] pb-[30px] overflow-hidden'>
-        {/* background image */}
         <Image
           src='/investor-relations/new/ir-sustain-bg.webp'
           alt='Sustainability Background'
@@ -78,27 +76,34 @@ export function SustainabilityManagementPage() {
       <ESGPolicy />
 
       <section className='space-y-[40px] py-[88px] bg-white'>
-        <Animation className='max-w-[960px] w-full mx-auto'>
-          <div className='relative pt-[86.88%] w-full '>
-            <Image src='/investor-relations/sustainable-1.png' alt='' fill />
-          </div>
-        </Animation>
-        <Animation className='max-w-[969px] w-full mx-auto'>
-          <div className='relative pt-[49.85%] w-full'>
-            <Image
-              src='/investor-relations/sustainable-2.jpeg'
-              alt='sustainable image 2'
-              fill
-            />
-          </div>
-        </Animation>
         <Animation className='max-w-[965px] w-full mx-auto'>
-          <div className='relative pt-[95.03%] w-full'>
-            <Image
-              src='/investor-relations/sustainable-3.png'
-              alt='sustainable image 3'
-              fill
-            />
+          <div className='relative w-full pt-[200%]'>
+            {locale === 'th' && (
+              <Image
+                src='/investor-relations/new/sustain-th.webp'
+                alt='sustain-th'
+                className='object-cover'
+                fill
+              />
+            )}
+
+            {locale === 'en' && (
+              <Image
+                src='/investor-relations/new/sustain-en.webp'
+                alt='sustain-en'
+                className='object-cover'
+                fill
+              />
+            )}
+
+            {locale === 'cn' && (
+              <Image
+                src='/investor-relations/new/sustain-cn.webp'
+                alt='sustain-cn'
+                className='object-cover'
+                fill
+              />
+            )}
           </div>
         </Animation>
       </section>
