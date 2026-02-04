3ef16c00f3e105f67e46e8eb9c49054832b675a2
Aphiwit1 <aphiwit@codework-tech.com>
2026-02-04 14:59:52 +0700

    feat(investorPage): allp page replay animation when click again

--- src/features/investorRelations/pages/ActivityPage/ActivityPage.tsx
+++ src/features/investorRelations/pages/ActivityPage/ActivityPage.tsx
@@ -3,17 +3,17 @@
 import { useTranslations } from 'next-intl'
 import { Menu } from '@/components/Menu'
 
-import { Banner } from '@/components/Banner'
-
-import { useState } from 'react'
-import { AccordionTabs } from '../../components/AccordionTabs'
 import { ActivityPageProps } from '@/features/investorRelations/pages/ActivityPage/interface'
-import SwiperVertical from '@/components/Header/components/BannerSwiper/BannerSwiper'
+
 import BannerImage from '@/components/Header/components/BannerImage/BannerImage'
 import { GroupStoryblok, TabStoryblok } from '@/types/storyblok'
+import GroupAccordionTabs from '@/features/investorRelations/components/GroupAccordionTabs/GroupAccordionTabs'
+import { useRouter } from '@/libs/intl/navigation'
 
+import { AnimatePresence } from 'framer-motion'
+import { Animation } from '@/components/Animation'
 export function ActivityPage({ data }: ActivityPageProps) {
-  const [openTabs, setOpenTabs] = useState<Record<number, number[]>>({})
+  useRouter()
   const tMenu = useTranslations('Menu')
 
   if (!data) {
@@ -22,54 +22,25 @@ export function ActivityPage({ data }: ActivityPageProps) {
 
   const group = (data.story.content.body[0].group as GroupStoryblok[]) || []
 
-  const toggleTab = (groupIndex: number, tabIndex: number) => {
-    setOpenTabs(prev => {
-      const groupOpenTabs = prev[groupIndex] || []
-      const newOpenTabs = groupOpenTabs.includes(tabIndex)
-        ? groupOpenTabs.filter(i => i !== tabIndex)
-        : [...groupOpenTabs, tabIndex]
-      return { ...prev, [groupIndex]: newOpenTabs }
-    })
-  }
-
   return (
     <main className='pb-[176px] bg-white'>
-      <Menu />
+      <AnimatePresence mode='wait'>
+        <Animation key={`good-corporate-${Date.now()}`}>
+          <Menu />
+          <BannerImage
+            mobileImageSrc='/investor-relations/new/investor-banner-mobile-10.webp'
+            imageSrc='/investor-relations/new/investor-banner-10.webp'
+            alt='investor-banner-10'
+          />
 
-      <BannerImage
-        mobileImageSrc='/investor-relations/new/investor-banner-mobile-10.webp'
-        imageSrc='/investor-relations/new/investor-banner-10.webp'
-        alt='investor-banner-10'
-      />
-
-      <section className='px-5 pb-5  pt-[50px] md:pt-[100px] max-w-4xl mx-auto space-y-6'>
-        <h1 className='text-lg md:text-3xl mb-7 text-blue-400 text-center'>
-          {tMenu('investorRelations.Activity')}
-        </h1>
-        {group.map((groupItem: GroupStoryblok, groupIndex: number) => (
-          <div key={groupIndex} className=' rounded-md p-4'>
-            <h2 className=' text-left text-lg mb-7 text-blue-400'>
-              {groupItem.heading}
-            </h2>
-
-            <div className='space-y-4'>
-              {groupItem.tab?.map((tabItem: TabStoryblok, tabIndex: number) => {
-                const isOpen = openTabs[groupIndex]?.includes(tabIndex) || false
-                return (
-                  <AccordionTabs
-                    tabIndex={tabIndex}
-                    key={'activity-' + tabIndex}
-                    groupIndex={groupIndex}
-                    toggleTab={toggleTab}
-                    tabItem={tabItem}
-                    isOpen={isOpen}
-                  />
-                )
-              })}
-            </div>
-          </div>
-        ))}
-      </section>
+          <section className='px-5 pb-5  pt-[50px] md:pt-[100px] max-w-4xl mx-auto space-y-6'>
+            <h1 className='text-lg md:text-3xl mb-7 text-blue-400 text-center'>
+              {tMenu('investorRelations.Activity')}
+            </h1>
+            <GroupAccordionTabs group={group} pageKey='activity' />
+          </section>
+        </Animation>
+      </AnimatePresence>
     </main>
   )
 }
--- src/features/investorRelations/pages/FinancialInformationPage/FinancialInformationPage.tsx
+++ src/features/investorRelations/pages/FinancialInformationPage/FinancialInformationPage.tsx
@@ -3,20 +3,21 @@
 import { useTranslations } from 'next-intl'
 import { Menu } from '@/components/Menu'
 
-import { useState } from 'react'
-
 import { FinancialInformationPageProps } from '@/features/investorRelations/pages/FinancialInformationPage/interface'
-import { AccordionTabs } from '../../components/AccordionTabs'
 
 import BannerImage from '@/components/Header/components/BannerImage/BannerImage'
 import { GroupStoryblok, TabStoryblok } from '@/types/storyblok'
+import GroupAccordionTabs from '@/features/investorRelations/components/GroupAccordionTabs/GroupAccordionTabs'
+import { useRouter } from '@/libs/intl/navigation'
+
+import { AnimatePresence } from 'framer-motion'
+import { Animation } from '@/components/Animation'
 
 export function FinancialInformationPage({
   financialInformationData,
 }: FinancialInformationPageProps) {
-  const t = useTranslations('InvestorInformationPage.FinancialInformation')
+  useRouter()
 
-  const [openTabs, setOpenTabs] = useState<Record<number, number[]>>({})
   const tMenu = useTranslations('Menu')
 
   if (!financialInformationData) {
@@ -26,53 +27,39 @@ export function FinancialInformationPage({
   const group =
     (financialInformationData.story.content.body[0]
       .group as GroupStoryblok[]) || []
-  const toggleTab = (groupIndex: number, tabIndex: number) => {
-    setOpenTabs(prev => {
-      const groupOpenTabs = prev[groupIndex] || []
-      const newOpenTabs = groupOpenTabs.includes(tabIndex)
-        ? groupOpenTabs.filter(i => i !== tabIndex)
-        : [...groupOpenTabs, tabIndex]
-      return { ...prev, [groupIndex]: newOpenTabs }
-    })
-  }
 
   return (
     <main className='pb-[176px] bg-white'>
-      <Menu />
-      <BannerImage
-        mobileImageSrc='/investor-relations/new/investor-banner-mobile-6.webp'
-        imageSrc='/investor-relations/new/investor-banner-6.webp'
-        alt='investor-banner-6'
-      />
+      <AnimatePresence mode='wait'>
+        <Animation key={`financial-information-${Date.now()}`}>
+          <Menu />
+          <BannerImage
+            mobileImageSrc='/investor-relations/new/investor-banner-mobile-6.webp'
+            imageSrc='/investor-relations/new/investor-banner-6.webp'
+            alt='investor-banner-6'
+          />
 
-      <section className='px-5 pb-5  pt-[50px] md:pt-[100px] max-w-4xl mx-auto space-y-6'>
-        <h1 className='text-lg md:text-3xl mb-10 text-blue-400 text-center'>
-          {tMenu('investorRelations.FinancialInformation')}
-        </h1>
-        {group.map((groupItem: GroupStoryblok, groupIndex: number) => (
-          <div key={groupIndex} className=' rounded-md p-4'>
-            <h2 className='   text-left text-lg mb-7 text-blue-400 '>
-              {groupItem.heading}
-            </h2>
+          <section className='px-5 pb-5  pt-[50px] md:pt-[100px] max-w-4xl mx-auto space-y-6'>
+            <h1 className='text-lg md:text-3xl mb-10 text-blue-400 text-center'>
+              {tMenu('investorRelations.FinancialInformation')}
+            </h1>
+            {group.map((groupItem: GroupStoryblok, groupIndex: number) => (
+              <div key={groupIndex} className=' rounded-md p-4'>
+                <h2 className='   text-left text-lg mb-7 text-blue-400 '>
+                  {groupItem.heading}
+                </h2>
 
-            <div className='space-y-4'>
-              {groupItem.tab?.map((tabItem: TabStoryblok, tabIndex: number) => {
-                const isOpen = openTabs[groupIndex]?.includes(tabIndex) || false
-                return (
-                  <AccordionTabs
-                    tabIndex={tabIndex}
-                    key={'financial-' + tabIndex}
-                    groupIndex={groupIndex}
-                    toggleTab={toggleTab}
-                    tabItem={tabItem}
-                    isOpen={isOpen}
+                <div className='space-y-4'>
+                  <GroupAccordionTabs
+                    pageKey='financial-information'
+                    group={group}
                   />
-                )
-              })}
-            </div>
-          </div>
-        ))}
-      </section>
+                </div>
+              </div>
+            ))}
+          </section>
+        </Animation>
+      </AnimatePresence>
     </main>
   )
 }
