41c709654ced2c4317d04d72feb060eeb738a85a
Aphiwit1 <aphiwit@codework-tech.com>
2026-02-06 14:49:12 +0700

    fix(Image): delete unused img #2

--- src/features/aboutUs/components/Slogan/index.tsx
+++ src/features/aboutUs/components/Slogan/index.tsx
@@ -6,10 +6,12 @@ export function Slogan() {
   const t = useTranslations('AboutUsPage.VisionMissionValues.Slogan')
 
   return (
-    <section className='bg-[url("/background.jpeg")] bg-center bg-cover mt-[120px]'>
+    <section className='bg-[url("/background.jpg")] bg-center bg-cover mt-[120px]'>
       <section className='bg-modellBgDark/60 text-white py-[116px]'>
         <Animation className='max-w-[580px] w-full mx-auto'>
-          <p className='headline-3 text-center whitespace-pre-line'>{t('title')}</p>
+          <p className='headline-3 text-center whitespace-pre-line'>
+            {t('title')}
+          </p>
         </Animation>
       </section>
     </section>
