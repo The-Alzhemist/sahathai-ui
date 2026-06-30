bea18b14878d668b196e5e313f3fb29639146985
pot-codework <chaiwat@codework-tech.com>
2026-06-30 11:57:01 +0700

    fix(operation-guidelines): remove phone number and email

--- src/features/infrastructureContainers/components/OperationGuidelines/components/DocumentProcess.tsx
+++ src/features/infrastructureContainers/components/OperationGuidelines/components/DocumentProcess.tsx
@@ -67,174 +67,106 @@ const DocumentProcessTH = () => {
           เทอร์มินอล เพื่อป้องกันความล่าช้า
         </li>
       </ul>
-
-      <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-2 mt-5'>
-        <span className='text-sm text-gray-600'>
-          ส่งอีเมล์ไปยังสำนักงานจัดเตรียมเอกสาร ที่
-        </span>
-        <span className='text-primary-1'>cr.stt@sahathai.com</span>
-      </div>
-
-      <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-2 mt-5'>
-        <span className='text-sm text-gray-600'>
-          ส่งอีเมล์ไปยังสำนักงานปฏิบัติการ STT ที่
-        </span>
-        <span className='text-primary-1'>planner.stt@sahathai.com</span>
-      </div>
     </div>
   )
 }
 
 const DocumentProcessEN = () => {
   return (
-    <>
-      <div className='px-5 text-sm text-gray-600'>
-        <h3 className='text-sm text-gray-800 mb-2 underline'>
-          Ship Planning Procedures
-        </h3>
+    <div className='px-5 text-sm text-gray-600'>
+      <h3 className='text-sm text-gray-800 mb-2 underline'>
+        Ship Planning Procedures
+      </h3>
 
-        <h4 className='text-sm text-gray-800 mb-2 underline'>
-          For Inbound Ship Planning
-        </h4>
-        <ul className='flex flex-col gap-y-5 list-disc mb-5'>
-          <li>
-            Not later than 12 hours prior to ETA, the shipping line or agent
-            must submit the EDI (Electronic Data Interchange) (Baplie)/CASP and
-            Special Cargo lists to the STT Planner.
-          </li>
-          <li>
-            Shipping lines must submit the Manifest, Inward Container, DG and
-            Special Cargo lists to STT’s Documentation Office.
-          </li>
-        </ul>
+      <h4 className='text-sm text-gray-800 mb-2 underline'>
+        For Inbound Ship Planning
+      </h4>
+      <ul className='flex flex-col gap-y-5 list-disc mb-5'>
+        <li>
+          Not later than 12 hours prior to ETA, the shipping line or agent must
+          submit the EDI (Electronic Data Interchange) (Baplie)/CASP and Special
+          Cargo lists to the STT Planner.
+        </li>
+        <li>
+          Shipping lines must submit the Manifest, Inward Container, DG and
+          Special Cargo lists to STT’s Documentation Office.
+        </li>
+      </ul>
 
-        <h4 className='text-sm text-gray-800 mb-2 underline'>
-          For Outbound Ship Planning
-        </h4>
-        <ul className='flex flex-col gap-y-5 list-disc'>
-          <li>
-            Not later than 12 hours prior to ETA the shipping line or agent must
-            submit the Booking Forecast, Pre-loading Plan and Special Stowage
-            Instructions to the STT Planner.
-          </li>
-          <li>
-            Not later than 12 hours prior to ETA, the shipping line or agent
-            must submit the Final Loading List and Final Loading Summary to the
-            STT Planner, in addition to the Final Loading Plan (if any).
-          </li>
-          <li>
-            Soon after the CY (Container Yard) cutoff time, the shipping line or
-            agent must confirm the Shut-Out Containers list with the STT
-            Planner.
-          </li>
-          <li>
-            In cases of full terminal booking, the shipping line or agent must
-            submit to the STT Planner a waiting list of containers to avoid
-            delays on shut-out decisions.
-          </li>
-        </ul>
-
-        <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-2 mt-5'>
-          <span className='text-gray-600'>
-            Email the Documentation Office at
-          </span>
-          <a
-            href='mailto:cr.stt@sahathai.com'
-            className='text-primary-1 underline'
-          >
-            cr.stt@sahathai.com
-          </a>
-        </div>
-
-        <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-2 mt-3'>
-          <span className='text-gray-600'>
-            Email the STT Operations Office at
-          </span>
-          <a
-            href='mailto:planner.stt@sahathai.com'
-            className='text-primary-1 underline'
-          >
-            planner.stt@sahathai.com
-          </a>
-        </div>
-      </div>
-    </>
+      <h4 className='text-sm text-gray-800 mb-2 underline'>
+        For Outbound Ship Planning
+      </h4>
+      <ul className='flex flex-col gap-y-5 list-disc'>
+        <li>
+          Not later than 12 hours prior to ETA the shipping line or agent must
+          submit the Booking Forecast, Pre-loading Plan and Special Stowage
+          Instructions to the STT Planner.
+        </li>
+        <li>
+          Not later than 12 hours prior to ETA, the shipping line or agent must
+          submit the Final Loading List and Final Loading Summary to the STT
+          Planner, in addition to the Final Loading Plan (if any).
+        </li>
+        <li>
+          Soon after the CY (Container Yard) cutoff time, the shipping line or
+          agent must confirm the Shut-Out Containers list with the STT Planner.
+        </li>
+        <li>
+          In cases of full terminal booking, the shipping line or agent must
+          submit to the STT Planner a waiting list of containers to avoid delays
+          on shut-out decisions.
+        </li>
+      </ul>
+    </div>
   )
 }
 
 const DocumentProcessCN = () => {
   return (
-    <>
-      <div className='px-5 text-sm text-gray-600'>
-        <h3 className='text-sm text-gray-800 mb-2 underline'>
-          Ship Planning Procedures
-        </h3>
+    <div className='px-5 text-sm text-gray-600'>
+      <h3 className='text-sm text-gray-800 mb-2 underline'>
+        Ship Planning Procedures
+      </h3>
 
-        <h4 className='text-sm text-gray-800 mb-2 underline'>
-          For Inbound Ship Planning
-        </h4>
-        <ul className='flex flex-col gap-y-5 list-disc mb-5'>
-          <li>
-            Not later than 12 hours prior to ETA, the shipping line or agent
-            must submit the EDI (Electronic Data Interchange) (Baplie)/CASP and
-            Special Cargo lists to the STT Planner.
-          </li>
-          <li>
-            Shipping lines must submit the Manifest, Inward Container, DG and
-            Special Cargo lists to STT’s Documentation Office.
-          </li>
-        </ul>
+      <h4 className='text-sm text-gray-800 mb-2 underline'>
+        For Inbound Ship Planning
+      </h4>
+      <ul className='flex flex-col gap-y-5 list-disc mb-5'>
+        <li>
+          Not later than 12 hours prior to ETA, the shipping line or agent must
+          submit the EDI (Electronic Data Interchange) (Baplie)/CASP and Special
+          Cargo lists to the STT Planner.
+        </li>
+        <li>
+          Shipping lines must submit the Manifest, Inward Container, DG and
+          Special Cargo lists to STT’s Documentation Office.
+        </li>
+      </ul>
 
-        <h4 className='text-sm text-gray-800 mb-2 underline'>
-          For Outbound Ship Planning
-        </h4>
-        <ul className='flex flex-col gap-y-5 list-disc'>
-          <li>
-            Not later than 12 hours prior to ETA the shipping line or agent must
-            submit the Booking Forecast, Pre-loading Plan and Special Stowage
-            Instructions to the STT Planner.
-          </li>
-          <li>
-            Not later than 12 hours prior to ETA, the shipping line or agent
-            must submit the Final Loading List and Final Loading Summary to the
-            STT Planner, in addition to the Final Loading Plan (if any).
-          </li>
-          <li>
-            Soon after the CY (Container Yard) cutoff time, the shipping line or
-            agent must confirm the Shut-Out Containers list with the STT
-            Planner.
-          </li>
-          <li>
-            In cases of full terminal booking, the shipping line or agent must
-            submit to the STT Planner a waiting list of containers to avoid
-            delays on shut-out decisions.
-          </li>
-        </ul>
-
-        <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-2 mt-5'>
-          <span className='text-gray-600'>
-            Email the Documentation Office at
-          </span>
-          <a
-            href='mailto:cr.stt@sahathai.com'
-            className='text-primary-1 underline'
-          >
-            cr.stt@sahathai.com
-          </a>
-        </div>
-
-        <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-2 mt-3'>
-          <span className='text-gray-600'>
-            Email the STT Operations Office at
-          </span>
-          <a
-            href='mailto:planner.stt@sahathai.com'
-            className='text-primary-1 underline'
-          >
-            planner.stt@sahathai.com
-          </a>
-        </div>
-      </div>
-    </>
+      <h4 className='text-sm text-gray-800 mb-2 underline'>
+        For Outbound Ship Planning
+      </h4>
+      <ul className='flex flex-col gap-y-5 list-disc'>
+        <li>
+          Not later than 12 hours prior to ETA the shipping line or agent must
+          submit the Booking Forecast, Pre-loading Plan and Special Stowage
+          Instructions to the STT Planner.
+        </li>
+        <li>
+          Not later than 12 hours prior to ETA, the shipping line or agent must
+          submit the Final Loading List and Final Loading Summary to the STT
+          Planner, in addition to the Final Loading Plan (if any).
+        </li>
+        <li>
+          Soon after the CY (Container Yard) cutoff time, the shipping line or
+          agent must confirm the Shut-Out Containers list with the STT Planner.
+        </li>
+        <li>
+          In cases of full terminal booking, the shipping line or agent must
+          submit to the STT Planner a waiting list of containers to avoid delays
+          on shut-out decisions.
+        </li>
+      </ul>
+    </div>
   )
 }
