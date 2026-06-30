ac77ac2b23383ef9704ca743690ecf6083e831d8
pot-codework <chaiwat@codework-tech.com>
2026-06-30 17:43:25 +0700

    fix(Liecense): add description

--- src/features/home/pages/HomePage/components/License/index.tsx
+++ src/features/home/pages/HomePage/components/License/index.tsx
@@ -2,27 +2,21 @@ import { useTranslations } from 'next-intl'
 import Image from 'next/image'
 
 import { Animation } from '@/components/Animation'
-import { DoubleQuoteIcon } from '@/components/icons/DoubleQuoteIcon'
 import { LicenseCard } from '../LicenseCard'
 
 export function License() {
   const t = useTranslations('HomePage.License')
   return (
     <section className='relative container min-h-[550px] flex flex-col justify-center items-center py-10 md:py-0'>
-      <div className="absolute inset-0 -z-10">
+      <div className='absolute inset-0 -z-10'>
         <Image
-          src="/home/license-bg.webp"
-          alt="lisense background"
+          src='/home/license-bg.webp'
+          alt='lisense background'
           fill
-          className="object-cover"
+          className='object-cover'
           priority
         />
       </div>
-      {/*<DoubleQuoteIcon*/}
-      {/*  className='text-secondary mx-auto mb-[59px]'*/}
-      {/*  width='36'*/}
-      {/*  height='17'*/}
-      {/*/>*/}
 
       <h2 className='headline-2 text-navy text-center mb-[10px]'>
         {t('title')}
@@ -32,39 +26,34 @@ export function License() {
       </p>
       <Animation className='flex flex-wrap justify-center gap-[15px]'>
         <LicenseCard
-          contentClassName='mt-[23px]'
           imageUrl='/home/license1.png'
           width={116}
           height={71}
           content={t('iso')}
         />
         <LicenseCard
-          contentClassName='mt-[8px]'
           imageUrl='/home/license2.png'
           width={58.67}
           height={88}
-          content={t('iso')}
+          content={t('globalSecurityVerification')}
         />
         <LicenseCard
-          contentClassName='mt-[6px]'
           imageUrl='/home/license3.png'
           width={151.01}
           height={88}
-          content={t('iso')}
+          content={t('thaiInvestorAssociation')}
         />
         <LicenseCard
-          contentClassName='mt-[6px]'
           imageUrl='/home/license4.png'
           width={129.68}
           height={88}
-          content={t('iso')}
+          content={t('sustainability')}
         />
         <LicenseCard
-          contentClassName='mt-[22px]'
           imageUrl='/home/license5.png'
           width={141}
           height={56}
-          content={t('iso')}
+          content={t('acknowledgements')}
         />
       </Animation>
     </section>
--- src/features/home/pages/HomePage/components/LicenseCard/index.tsx
+++ src/features/home/pages/HomePage/components/LicenseCard/index.tsx
@@ -4,21 +4,17 @@ import { LicenseCardProps } from './interface'
 import { cn } from '@/libs/util'
 
 export function LicenseCard({
-  contentClassName,
   imageUrl,
   content,
   width,
   height,
 }: LicenseCardProps) {
   return (
-    <div className='flex flex-col justify-end items-center bg-white border-t-[10px] border-background py-[16px] px-[4px] shadow-4 w-[160px]'>
-      <Image src={imageUrl} width={width} height={height} alt='' />
-      <p
-        className={cn(
-          'text-black-6 caption text-center max-w-[101px] mx-auto',
-          contentClassName
-        )}
-      >
+    <div className='flex flex-col items-center bg-white border-t-[10px] border-background py-[16px] px-[4px] shadow-4 w-[160px]'>
+      <div className='grid items-center h-[90px]'>
+        <Image src={imageUrl} width={width} height={height} alt={content} />
+      </div>
+      <p className='text-black-6 caption text-center mx-auto mt-[6px]'>
         {content}
       </p>
     </div>
