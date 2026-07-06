7206d07fa9896002b855640232bc6fa3826b66f3
pot-codework <chaiwat@codework-tech.com>
2026-07-06 18:05:10 +0700

    fix(ServiceProcessContent): move content to i18n

--- src/features/infrastructureContainers/components/OperationGuidelines/components/ServiceProcess.tsx
+++ src/features/infrastructureContainers/components/OperationGuidelines/components/ServiceProcess.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Image from 'next/image'
 import { useLocale } from 'next-intl'
 
+import { ServiceProcessContent } from './ServiceProcessContent'
+
 export default function ServiceProcess() {
   const locale = useLocale()
   return (
@@ -13,64 +15,7 @@ export default function ServiceProcess() {
         width={720}
         height={328}
       />
-      {locale === 'th' && <ServiceProcessTH />}
-      {locale === 'en' && <ServiceProcessEN />}
-      {locale === 'cn' && <ServiceProcessCN />}
+      <ServiceProcessContent />
     </section>
-  )
-}
-
-export const ServiceProcessTH = () => {
-  return (
-    <>
-      <p className='text-gray-600 text-sm mb-3'>
-        เรากำลังพัฒนาและปรับเปลี่ยนขั้นตอนการเรียกเก็บค่าใช้บริการที่สะดวกและรวดเร็วมากขึ้น
-      </p>
-
-      <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-2'>
-        <span className='text-sm text-gray-600'>
-          หากคุณมีข้อสงสัยหรือต้องการทราบข้อมูลเพิ่มเติม สามารถติดต่อเราได้ที่
-        </span>
-        <span className='text-primary-1'>+66 (0) 2386 8000.</span>
-      </div>
-    </>
-  )
-}
-
-export const ServiceProcessEN = () => {
-  return (
-    <>
-      <p className='text-gray-600 text-sm mb-3'>
-        We are currently working on billing procedures that would help
-        streamline invoicing and receipts. Should you have any inquiries
-        regarding billing procedures
-      </p>
-
-      <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-2'>
-        <span className='text-sm text-gray-600'>
-          please feel free to contact us at
-        </span>
-        <span className='text-primary-1'>+66 (0) 2386 8000.</span>
-      </div>
-    </>
-  )
-}
-
-export const ServiceProcessCN = () => {
-  return (
-    <>
-      <p className='text-gray-600 text-sm mb-3'>
-        We are currently working on billing procedures that would help
-        streamline invoicing and receipts. Should you have any inquiries
-        regarding billing procedures
-      </p>
-
-      <div className='flex flex-wrap gap-2 border rounded-md items-center px-3 py-2'>
-        <span className='text-sm text-gray-600'>
-          please feel free to contact us at
-        </span>
-        <span className='text-primary-1'>+66 (0) 2386 8000.</span>
-      </div>
-    </>
   )
 }
