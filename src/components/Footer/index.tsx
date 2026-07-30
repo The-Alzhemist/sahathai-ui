1036e6e61392b2689d711e3245b80d8215d352f7
pot-codework <chaiwat@codework-tech.com>
2026-07-30 14:02:06 +0700

    fix(footer): layout

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,34 +8,34 @@ export function Footer() {
 
   return (
     <footer className='flex flex-col justify-between py-6 px-10 bg-blue-400 text-white body-s'>
-      <section className='flex justify-between w-full mb-5'>
-        <span> info@sahathaiterminal.com @2025. All rights reserved.</span>
-        <div className='flex gap-x-[16px]'>
-          <Link
-            href='https://www.youtube.com/channel/UCp2XYZiyDSN6Xt5DSQv_foA/videos?view=0&sort=dd&shelf_id=0'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <YoutubeIcon width='24' height='24' />
-          </Link>
-          <Link
-            href='https://www.facebook.com/sahathaiterminal/?locale=th_TH'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FacebookIcon width='24' height='24' />
-          </Link>
-          <Link
-            href='https://www.instagram.com/sahathaiterminal/?hl=en'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <InstagramIcon width='24' height='24' />
-          </Link>
-        </div>
+      <section className='flex justify-center gap-x-[16px] mb-2'>
+        <Link
+          href='https://www.youtube.com/channel/UCp2XYZiyDSN6Xt5DSQv_foA/videos?view=0&sort=dd&shelf_id=0'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <YoutubeIcon width='24' height='24' />
+        </Link>
+        <Link
+          href='https://www.facebook.com/sahathaiterminal/?locale=th_TH'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FacebookIcon width='24' height='24' />
+        </Link>
+        <Link
+          href='https://www.instagram.com/sahathaiterminal/?hl=en'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <InstagramIcon width='24' height='24' />
+        </Link>
+      </section>
+      <section className='w-full mb-2 text-center'>
+        info@sahathaiterminal.com @2025. All rights reserved.
       </section>
 
-      <section className='flex justify-start'>
+      <section className='flex justify-center'>
         <Link href='/privacy-policy'>{t('Privacy_policy')}</Link>
       </section>
     </footer>
